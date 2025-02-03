fae30db8d4628e4cc875e1851fa0ea96c8c11b73
Joseph Gordy <jgordy2424@gmail.com>
2025-02-03 15:30:40 -0500

    Refactor theme management by creating a separate theme file and updating primary color to green

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,8 @@ import { ColorSchemeScript, MantineProvider, createTheme } from '@mantine/core';
 import type { Route } from "./+types/root";
 import "./app.css";
 
+import theme from './theme';
+
 export const links: Route.LinksFunction = () => [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
     {
@@ -26,11 +28,6 @@ export const links: Route.LinksFunction = () => [
         href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
     },
 ];
-
-const theme = createTheme({
-    fontFamily: 'Open Sans, sans-serif',
-    primaryColor: 'cyan',
-});
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
---
+++ app/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mantine/core';
+
+const theme = createTheme({
+    fontFamily: 'Open Sans, sans-serif',
+    primaryColor: 'green',
+    colors: {
+        'green': ["#eafcef", "#d9f6df", "#b1ebbe", "#86e09a", "#63d77c", "#4dd269", "#40cf5e", "#31b74e", "#27a343", "#168d36"],
+    },
+});
+
+export default theme;
