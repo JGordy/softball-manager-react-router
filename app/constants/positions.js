dd799f94098749471a8cde06c1cf488dbdecb0fb
Joseph Gordy <jgordy2424@gmail.com>
2025-02-04 11:48:42 -0500

    Add player positions and sample player data

---
+++ app/constants/positions.js
@@ -0,0 +1,14 @@
+const positions = [
+    "Left Field",
+    "Left Center Field",
+    "Right Center Field",
+    "Right Field",
+    "Shortstop",
+    "Third Base",
+    "Second Base",
+    "First Base",
+    "Catcher",
+    "Pitcher"
+];
+
+export default positions;
