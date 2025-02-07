a5c05ebe2409cce39ab711286a3e9bee55d7a036
Joseph Gordy <jgordy2424@gmail.com>
2025-02-07 15:59:32 -0500

    Add database utility functions for document management

---
+++ app/utils/databases.js
@@ -0,0 +1,68 @@
+import { ID, databases } from '../appwrite.js';
+
+const databaseId = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+
+// Specific collection IDs
+export const collections = {
+    users: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+    teams: import.meta.env.VITE_APPWRITE_TEAMS_COLLECTION_ID,
+    memberships: import.meta.env.VITE_APPWRITE_MEMBERSHIPS_COLLECTION_ID,
+};
+
+console.log({ collections });
+
+// Helper function to create a document
+export const createDocument = async (collectionType, id, data) => {
+    const _id = id || ID.unique();
+    try {
+        const response = await databases.createDocument(databaseId, collections[collectionType], _id, data);
+        return response;
+    } catch (error) {
+        console.error(`Error creating ${collectionType} document:`, error);
+        throw error;
+    }
+};
+
+// Helper function to list a series of documents
+export const listDocuments = async (collectionType, queries) => {
+    try {
+        const response = await databases.listDocuments(databaseId, collections[collectionType], queries);
+        return response
+    } catch (error) {
+        console.error(`Error listing ${collectionType} documents:`, error);
+        throw error;
+    }
+}
+
+// Helper function to read a document
+export const readDocument = async (collectionType, documentId) => {
+    try {
+        const response = await databases.getDocument(databaseId, collections[collectionType], documentId);
+        return response;
+    } catch (error) {
+        console.error(`Error reading ${collectionType} document:`, error);
+        throw error;
+    }
+};
+
+// Helper function to update a document
+export const updateDocument = async (collectionType, documentId, data) => {
+    try {
+        const response = await databases.updateDocument(databaseId, collections[collectionType], documentId, data);
+        return response;
+    } catch (error) {
+        console.error(`Error updating ${collectionType} document:`, error);
+        throw error;
+    }
+};
+
+// Helper function to delete a document
+export const deleteDocument = async (collectionType, documentId) => {
+    try {
+        const response = await databases.deleteDocument(databaseId, collections[collectionType], documentId);
+        return response;
+    } catch (error) {
+        console.error(`Error deleting ${collectionType} document:`, error);
+        throw error;
+    }
+};
