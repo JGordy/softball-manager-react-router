82869d651b0ac852f20f965286b65388a33684e0
Joseph Gordy <jgordy2424@gmail.com>
2025-02-08 18:55:26 -0500

    Add TeamCard component to display team details in a card layout

---
+++ app/routes/user/components/TeamCard.jsx
@@ -0,0 +1,88 @@
+import {
+    Card,
+    Text,
+    Group,
+    ThemeIcon,
+} from '@mantine/core';
+
+import {
+    IconCalendar,
+    IconCurrencyDollar,
+    IconMapPin,
+} from '@tabler/icons-react';
+
+export default function TeamCard(team) {
+    console.log({ team });
+
+    return (
+        <Card
+            key={team.$id}
+            p="lg"
+            shadow="sm"
+            radius="md"
+            withBorder
+            // onClick={handleCardClick} // Add click handler
+            style={{ cursor: 'pointer' }}
+        >
+            <Card.Section>
+                <Text weight={500} size="lg">
+                    {team.name}
+                </Text>
+            </Card.Section>
+
+            <Card.Section mt="sm">
+                <Group spacing="xs">
+                    <ThemeIcon variant="light" color="blue">
+                        <IconMapPin size={14} />
+                    </ThemeIcon>
+                    <Text size="sm" color="dimmed">
+                        {team.location || "Location not specified"} {/* Handle null location */}
+                    </Text>
+                </Group>
+            </Card.Section>
+
+
+            <Card.Section mt="xs">
+                <Group spacing="xs">
+                    <ThemeIcon variant="light" color="green">
+                        <IconCalendar size={14} />
+                    </ThemeIcon>
+                    <Text size="sm" color="dimmed">
+                        {new Date(team.seasonStartDate).toLocaleDateString()} - {new Date(team.seasonEndDate).toLocaleDateString()}
+                    </Text>
+                </Group>
+            </Card.Section>
+
+            <Card.Section mt="xs">
+                <Group spacing="xs">
+                    <ThemeIcon variant="light" color="yellow">
+                        <IconCurrencyDollar size={14} />
+                    </ThemeIcon>
+                    <Text size="sm" color="dimmed">
+                        {team.signUpFee}
+                    </Text>
+                </Group>
+            </Card.Section>
+
+            <Card.Section mt="xs">
+                <Group spacing="xs">
+                    <Text size="sm" color="dimmed">
+                        {team.genderMix}
+                    </Text>
+                </Group>
+            </Card.Section>
+
+            <Card.Section mt="xs">
+                <Text size="sm" color="dimmed">
+                    League: {team.leagueName}
+                </Text>
+            </Card.Section>
+
+            <Card.Section mt="xs">
+                <Text size="sm" color="dimmed">
+                    Game Days: {team.gameDays}
+                </Text>
+            </Card.Section>
+        </Card>
+    );
+};
