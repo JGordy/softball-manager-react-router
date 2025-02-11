0fc15e2767251d94b6547f6a2b93b2ca053849d6
Joseph Gordy <jgordy2424@gmail.com>
2025-02-10 22:33:59 -0500

    Add PlayerForm component for team member input with validation

---
+++ app/routes/team/components/PlayerForm.jsx
@@ -0,0 +1,90 @@
+import { Form } from 'react-router';
+
+import {
+    Button,
+    Group,
+    Input,
+    MultiSelect,
+    Select,
+    TextInput,
+} from '@mantine/core';
+
+import { IMaskInput } from 'react-imask';
+
+// import { IconMan, IconWoman, IconFriends } from '@tabler/icons-react';
+
+import AutocompleteEmail from '@/components/AutoCompleteEmail';
+
+import positions from '@/constants/positions';
+
+import classes from '@/styles/inputs.module.css';
+
+export default function TeamForm({ setIsModalOpen, setError }) {
+
+    // const iconProps = {
+    //     color: 'currentColor',
+    //     size: 18,
+    // };
+
+    return (
+        <Form method="post">
+            <TextInput
+                className={classes.inputs}
+                label="First Name"
+                name="firstName"
+                required
+            />
+            <TextInput
+                className={classes.inputs}
+                label="Last Name"
+                name="lastName"
+                required
+            />
+            <AutocompleteEmail classes={classes.inputs} />
+            <Input.Wrapper className={classes.inputs}>
+                <Input.Label>Phone Number</Input.Label>
+                <Input
+                    component={IMaskInput}
+                    label="Phone Number"
+                    mask="(000) 000-0000"
+                    name="phoneNumber"
+                    placeholder='(xxx) xxx-xxxx'
+                />
+            </Input.Wrapper>
+            <Select
+                className={classes.inputs}
+                label="Gender"
+                placeholder="Select Gender"
+                data={['Male', 'Female']}
+                mb="sm"
+            />
+            <MultiSelect
+                className={classes.inputs}
+                label="Preferred Positions"
+                placeholder="Select Positions"
+                data={positions}
+                mb="sm"
+            />
+            <TextInput
+                className={classes.inputs}
+                label="Walk Up Song"
+                placeholder="Walk Up Song"
+                mb="sm"
+            />
+
+            <Group position="right" mt="lg">
+                <Button type="submit">Submit</Button>
+                <Button
+                    variant="outline"
+                    color="gray"
+                    onClick={() => {
+                        setIsModalOpen(false);
+                        setError(null);
+                    }}
+                >
+                    Cancel
+                </Button>
+            </Group>
+        </Form>
+    );
+};
