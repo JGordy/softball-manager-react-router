4c0a70db2190d83160f4b671c953b9e4cbe52bf6
Joseph Gordy <jgordy2424@gmail.com>
2025-02-10 22:34:05 -0500

    Add PlayerList component to display team players in a table format

---
+++ app/routes/team/components/PlayerList.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import {
+    Table,
+    ScrollArea,
+} from '@mantine/core'
+
+import styles from '@/styles/playerChart.module.css';
+
+export default function PlayerList({ players }) {
+    // console.log({ players });
+
+    const [scrolled, setScrolled] = useState(false);
+
+    const columns = [
+        {
+            accessor: 'initials',
+            title: '',
+        },
+        {
+            accessor: 'name',
+            title: 'Name',
+        },
+        {
+            accessor: 'positions',
+            title: 'Positions'
+        },
+        {
+            accessor: 'email',
+            title: 'Email',
+        },
+    ];
+
+    const headerClassName = scrolled ? styles.header + ' ' + styles.scrolled : styles.header;
+
+    return (
+        <div className={styles.tableContainer}>
+            <ScrollArea mih={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+                <Table striped highlightOnHover withTableBorder>
+                    <Table.Thead className={headerClassName}>
+                        <Table.Tr>
+                            {columns.map((column) => (
+                                <Table.Th key={column.accessor}>{column.title}</Table.Th>
+                            ))}
+                        </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>
+                        {players.map((row) => (
+                            <Table.Tr key={row.$id}>
+                                <Table.Td>{' '}</Table.Td>
+                                <Table.Td>{row.firstName + ' ' + row.lastName}</Table.Td>
+                                <Table.Td>{' '}</Table.Td>
+                                <Table.Td>{row.email}</Table.Td>
+                            </Table.Tr>
+                        ))}
+                    </Table.Tbody>
+                </Table>
+            </ScrollArea>
+        </div>
+    )
+};
