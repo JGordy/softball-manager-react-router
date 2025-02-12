0af1000e0f383b299f593acb79ad4e29f46ee48b
Joseph Gordy <jgordy2424@gmail.com>
2025-02-11 23:18:48 -0500

    Enhance TeamDetails component with additional icons and improved text properties for better readability

--- app/routes/team/details.jsx
+++ app/routes/team/details.jsx
@@ -14,7 +14,7 @@ import {
     Title,
 } from '@mantine/core';
 
-import { IconCurrencyDollar, IconCalendar, IconMapPin } from '@tabler/icons-react';
+import { IconCurrencyDollar, IconCalendar, IconMapPin, IconFriends, IconCalendarRepeat } from '@tabler/icons-react';
 
 import { listDocuments, readDocument } from '@/utils/databases';
 import { adjustColorBasedOnDarkness } from '@/utils/adjustHexColor';
@@ -69,68 +69,88 @@ export default function TeamDetails({ actionData, loaderData }) {
     const [teamDetails, setTeamDetails] = useState(teamData || {});
     const [error, setError] = useState(null);
 
-    let { primaryColor } = teamDetails;
+    const { primaryColor } = teamDetails;
     const adjustedColor = adjustColorBasedOnDarkness(primaryColor, 50);
 
     const iconProps = {
         variant: 'gradient',
         gradient: { from: primaryColor, to: adjustedColor, deg: 270 },
-        autoContrast: true,
+        size: 'lg',
+        // autoContrast: true,
     };
+
+    const textProps = {
+        size: "md",
+        // c: "dimmed",
+    }
 
     return (
         <Container size="xl" p="xl">
             <Title order={2} align="center" mt="sm" mb="lg">
                 {teamDetails.name}
             </Title>
+            <Text {...textProps} align="center">
+                {teamDetails.leagueName}
+            </Text>
             <Divider my="sm" />
 
             <Stack spacing="sm">
                 <Group spacing="xs">
                     <ThemeIcon {...iconProps}>
-                        <IconMapPin size={14} />
+                        <IconMapPin size={18} />
                     </ThemeIcon>
-                    <Text size="sm" c="dimmed">
+                    <Text {...textProps}>
                         {teamDetails.location || "Location not specified"}
                     </Text>
                 </Group>
 
                 <Group spacing="xs">
                     <ThemeIcon {...iconProps}>
-                        <IconCalendar size={14} />
+                        <IconCalendar size={18} />
                     </ThemeIcon>
-                    <Text size="sm" c="dimmed">
+                    <Text {...textProps}>
                         {new Date(teamDetails.seasonStartDate).toLocaleDateString()} - {new Date(teamDetails.seasonEndDate).toLocaleDateString()}
                     </Text>
                 </Group>
 
                 <Group spacing="xs">
                     <ThemeIcon {...iconProps}>
-                        <IconCurrencyDollar size={14} />
+                        <IconCurrencyDollar size={18} />
                     </ThemeIcon>
-                    <Text size="sm" c="dimmed">
+                    <Text {...textProps}>
                         {teamDetails.signUpFee}
                     </Text>
                 </Group>
 
                 <Group spacing="xs">
-                    <Text size="sm" c="dimmed">
+                    <ThemeIcon {...iconProps}>
+                        <IconFriends size={18} />
+                    </ThemeIcon>
+                    <Text {...textProps}>
                         {teamDetails.genderMix}
                     </Text>
                 </Group>
 
-                <Text size="sm" c="dimmed">
-                    League: {teamDetails.leagueName}
-                </Text>
-
-                <Text size="sm" c="dimmed">
-                    Game Days: {teamDetails.gameDays}
-                </Text>
+                <Group spacing="xs">
+                    <ThemeIcon {...iconProps}>
+                        <IconCalendarRepeat size={18} />
+                    </ThemeIcon>
+                    <Text {...textProps}>
+                        {`${teamDetails.gameDays}s`}
+                    </Text>
+                </Group>
             </Stack>
 
             <PlayerList players={players} />
 
-            <Button mt="md" onClick={() => setIsModalOpen(true)}>Add Player</Button>
+            <Button
+                mt="md"
+                color={primaryColor}
+                onClick={() => setIsModalOpen(true)}
+                autoContrast
+            >
+                Add Player
+            </Button>
 
             <Modal opened={isModalOpen} onClose={() => setIsModalOpen(false)} title="Add a Player">
                 {error && <Alert type="error" mb="md" c="red">{error}</Alert>}
