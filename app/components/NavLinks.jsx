21cd053d144b0d06add9d63abfbcb20fb93b0352
Joseph Gordy <jgordy2424@gmail.com>
2025-02-13 11:38:22 -0500

    Add NavLinks component for improved navigation and styling

---
+++ app/components/NavLinks.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams, useLocation } from 'react-router';
+
+import { Center, SegmentedControl } from '@mantine/core';
+
+import {
+    IconBallBaseball,
+    IconCalendar,
+    IconUserSquareRounded
+} from '@tabler/icons-react';
+
+import classes from '@/styles/navLinks.module.css';
+
+function NavLinks() {
+    const navigate = useNavigate();
+    const { userId } = useParams();
+    const location = useLocation();
+
+    const getInitialValue = () => {
+        const pathParts = location.pathname.split('/');
+        return pathParts.includes('teams') ? 'teams' : 'profile';
+    };
+
+    const [value, setValue] = useState(getInitialValue());
+
+    const links = [
+        {
+            label: (
+                <Center style={{ gap: 10 }}>
+                    <IconUserSquareRounded size={24} />
+                    <span>Profile</span>
+                </Center>
+            ),
+            value: 'profile'
+
+        },
+        {
+            label: (
+                <Center style={{ gap: 10 }}>
+                    <IconBallBaseball size={24} />
+                    <span>Teams</span>
+                </Center>
+            ),
+            value: 'teams'
+
+        },
+        {
+            label: (
+                <Center style={{ gap: 10 }}>
+                    <IconCalendar size={24} />
+                    <span>Schedule</span>
+                </Center>
+            ),
+            value: 'schedule',
+            disabled: true,
+        },
+    ]
+
+    useEffect(() => {
+        setValue(getInitialValue()); // Update value when location changes
+    }, [location]); // Add location as a dependency
+
+    const handleNavLinkClick = (newValue) => {
+        setValue(newValue);
+        let href = `/user/${userId}`;
+        if (newValue === 'profile') {
+            navigate(href);
+        } else {
+            navigate(`${href}/${newValue}`);
+        }
+    };
+
+    return (
+        <div className={classes.navLinksContainer}>
+            <SegmentedControl
+                className={classes.navLinks}
+                color="green"
+                data={links}
+                fullWidth
+                onChange={handleNavLinkClick}
+                size="lg"
+                radius="xl"
+                value={value}
+                transitionDuration={500}
+                transitionTimingFunction="linear"
+                withItemsBorders={false}
+            />
+        </div>
+    );
+}
+
+export default NavLinks;
