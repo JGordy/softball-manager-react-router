53a0f6f8938288c6f6a8e7b0772459f3e9f2880b
Joseph Gordy <jgordy2424@gmail.com>
2025-02-13 16:16:54 -0500

    Refactor UserProfile component to enhance layout with Card and List elements, improving readability and structure

--- app/routes/user/profile.jsx
+++ app/routes/user/profile.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react';
 import {
     Alert,
     Avatar,
+    Card,
     Container,
     Divider,
     Group,
+    List,
     Text,
     Title,
-    ThemeIcon,
+    // ThemeIcon,
     Stack,
 } from '@mantine/core';
 
@@ -24,19 +26,19 @@ import { getProfile } from "./loader";
 
 const fieldsToDisplay = {
     email: {
-        icon: <IconMail size={28} />,
+        icon: <IconMail size={20} />,
         label: 'email',
     },
     phoneNumber: {
-        icon: <IconPhone size={28} />,
+        icon: <IconPhone size={20} />,
         label: 'phone number',
     },
     gender: {
-        icon: <IconFriends size={28} />,
+        icon: <IconFriends size={20} />,
         label: 'gender',
     },
     walkUpSong: {
-        icon: <IconHeadphonesFilled size={28} />,
+        icon: <IconHeadphonesFilled size={20} />,
         label: ' walk up song',
     },
 };
@@ -71,14 +73,61 @@ export default function UserProfile({ loaderData }) {
     return (
         <Container>
             <Title order={2} align="center" mt="sm" mb="lg">My Profile</Title>
+
+            <Card shadow="sm" padding="lg" radius="xl" withBorder>
+                <Group position="center" mb="lg">
+                    <Avatar color="green" name={fullName} alt={fullName} />
+                    <div>
+                        <Title order={3}>{fullName}</Title>
+                    </div>
+                </Group>
+
+                <Divider my="md" />
+
+                <Stack>
+                    <Title order={4}>Preferred Positions</Title>
+                    <Group position="center" mb="lg">
+                        {!preferredPositions.length ? (
+                            <Text c="red">None listed*</Text>
+                        ) : preferredPositions?.map(position => (
+                            <Avatar color="initials" name={position} alt={position} key={position} />
+                        ))}
+                    </Group>
+                </Stack>
+            </Card>
+
+
+            <Card shadow="sm" padding="lg" radius="xl" mt="md" withBorder>
+                <List
+                    spacing="xs"
+                    size="sm"
+                    center
+                >
+                    {Object.entries(fieldsToDisplay).map(([key, { icon, label }]) => {
+                        const value = restOfData[key];
+                        return (
+                            <List.Item key={key} icon={icon}>
+                                {/* <ThemeIcon size="lg">
+                                {icon}
+                                </ThemeIcon> */}
+                                <Text size="sm" c={!value ? 'red' : ''}>
+                                    {value || `${label} not listed*`}
+                                </Text>
+                            </List.Item>
+                        );
+                    })}
+                </List>
+            </Card>
+
+
             {showAlert && (
                 <Alert
-                    mb="20px"
+                    mt="20px"
                     autoContrast
                     color="orange"
                     icon={<IconInfoCircle />}
                     onClose={handleAlertClose}
-                    radius="md"
+                    radius="xl"
                     title="Your profile is incomplete!"
                     withCloseButton={true}
                 >
@@ -90,44 +139,6 @@ export default function UserProfile({ loaderData }) {
                     </ol>
                 </Alert>
             )}
-
-            <Group position="center" mb="lg">
-                <Avatar color="initials" name={fullName} alt={fullName} />
-                <div>
-                    <Title order={3}>{fullName}</Title>
-                </div>
-            </Group>
-
-            <Divider my="md" />
-
-            <Stack>
-                <Title order={4}>Preferred Positions</Title>
-                <Group position="center" mb="lg">
-                    {!preferredPositions.length ? (
-                        <Text c="red">None listed*</Text>
-                    ) : preferredPositions?.map(position => (
-                        <Avatar color="initials" name={position} alt={position} key={position} />
-                    ))}
-                </Group>
-            </Stack>
-
-            <Divider my="md" />
-
-            <Stack spacing="xs">
-                {Object.entries(fieldsToDisplay).map(([key, { icon, label }]) => {
-                    const value = restOfData[key];
-                    return (
-                        <Group spacing="xs">
-                            {/* <ThemeIcon size="lg"> */}
-                            {icon}
-                            {/* </ThemeIcon> */}
-                            <Text size="md" c={!value ? 'red' : ''}>
-                                {value || `${label} not listed*`}
-                            </Text>
-                        </Group>
-                    );
-                })}
-            </Stack>
         </Container>
     );
 }
