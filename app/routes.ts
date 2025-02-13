3194633169e157dc1d83340b77597f0a4d7e9ccc
Joseph Gordy <jgordy2424@gmail.com>
2025-02-13 11:39:17 -0500

    Refactor user routes to separate user profile and teams functionality

--- app/routes.ts
+++ app/routes.ts
@@ -26,13 +26,14 @@ export default [
         // route("/forgot-password", "routes/auth/recover.jsx"),
 
         // User routes
-        route('/user/:userId', 'routes/user/dashboard.jsx'),
+        route('/user/:userId', 'routes/user/profile.jsx'),
+        route('/user/:userId/teams', 'routes/user/teams.jsx'),
+
+        // Team routes
+        route('/user/:userId/teams/:teamId', 'routes/team/details.jsx'),
 
         // Gameday Routes
         route('/gameday/lineup', 'routes/gameday/lineup.jsx'),
-
-        // Team routes
-        route('/teams/:teamId', 'routes/team/details.jsx'),
 
         // Api routes
         route('/api/generate/lineup', 'routes/api/generate/lineup.js'),
