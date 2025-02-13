fe1207e282b129fedb1be85f495931195a0f0088
Joseph Gordy <jgordy2424@gmail.com>
2025-02-12 22:42:55 -0500

    Add createTeam action to handle team creation and membership assignment

---
+++ app/routes/team/action.js
@@ -0,0 +1,31 @@
+import { ID } from '@/appwrite';
+import { createDocument } from '@/utils/databases.js';
+
+export async function createTeam({ request, params }) {
+    console.log({ request, params });
+    const { userId } = params;
+
+    const formData = await request.formData();
+    const teamData = Object.fromEntries(formData.entries());
+
+    console.log('createTeamAction > teamData: ', { teamData });
+
+    try {
+        const teamId = ID.unique(); // Create this now so it's easier to use later
+
+        const team = await createDocument(
+            'teams', // Your teams collection ID
+            teamId, // Generates a unique team ID in the handler
+            { ...teamData, signUpFee: Number(teamData.signUpFee) },
+        );
+
+        // Create document in relationship table for the user and team id's. Assume the user creating the team is a coach
+        const membership = await createDocument('memberships', null, { userId, teamId, role: 'coach' });
+        console.log({ membership });
+
+        return { response: team, status: 200 };
+    } catch (error) {
+        console.error("Error creating team:", error);
+        throw error;
+    }
+}
