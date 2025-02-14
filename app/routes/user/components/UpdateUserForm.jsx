4bd0e864d0272caf6f8dca53953dbf4a5d2ab9db
Joseph Gordy <jgordy2424@gmail.com>
2025-02-13 23:34:41 -0500

    Add UpdateUserForm component for user data updates with validation

---
+++ app/routes/user/components/UpdateUserForm.jsx
@@ -0,0 +1,89 @@
+import { Form } from 'react-router';
+
+import {
+    Button,
+    Group,
+    Input,
+    MultiSelect,
+    Select,
+    TextInput,
+} from '@mantine/core';
+
+import { IMaskInput } from 'react-imask';
+
+import AutocompleteEmail from '@/components/AutoCompleteEmail';
+
+import positions from '@/constants/positions';
+
+import classes from '@/styles/inputs.module.css';
+
+export default function UpdateUserForm({ user, setIsModalOpen, setError }) {
+
+    return (
+        <Form method="post">
+            <TextInput
+                className={classes.inputs}
+                label="First Name"
+                name="firstName"
+            />
+            <TextInput
+                className={classes.inputs}
+                label="Last Name"
+                name="lastName"
+            />
+            <AutocompleteEmail classes={classes.inputs} />
+            <Input.Wrapper className={classes.inputs}>
+                <Input.Label>Phone Number</Input.Label>
+                <Input
+                    component={IMaskInput}
+                    defaultValue={user.phoneNumber}
+                    label="Phone Number"
+                    mask="(000) 000-0000"
+                    name="phoneNumber"
+                    placeholder='(xxx) xxx-xxxx'
+                />
+            </Input.Wrapper>
+            <Select
+                className={classes.inputs}
+                label="Gender"
+                name="gender"
+                placeholder="Select Gender"
+                data={['Male', 'Female']}
+                mb="sm"
+            />
+            <MultiSelect
+                className={classes.inputs}
+                label="Preferred Positions"
+                name="preferredPositions"
+                placeholder="Select Positions"
+                data={positions}
+                mb="sm"
+                clearable
+                searchable
+            />
+            <TextInput
+                className={classes.inputs}
+                label="Walk Up Song"
+                name="walkUpSong"
+                placeholder="Walk Up Song"
+                mb="sm"
+            />
+
+            <Group position="right" mt="lg">
+                <Button type="submit">
+                    Submit
+                </Button>
+                <Button
+                    variant="outline"
+                    color="gray"
+                    onClick={() => {
+                        setIsModalOpen(false);
+                        setError(null);
+                    }}
+                >
+                    Cancel
+                </Button>
+            </Group>
+        </Form>
+    );
+};
