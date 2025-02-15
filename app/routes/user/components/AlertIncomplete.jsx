429cb0ffbeac9a8776365d7a8349a39d23b76b57
Joseph Gordy <jgordy2424@gmail.com>
2025-02-14 22:27:11 -0500

    Add AlertIncomplete component to notify users of incomplete profile data

---
+++ app/routes/user/components/AlertIncomplete.jsx
@@ -0,0 +1,24 @@
+import { Alert } from "@mantine/core";
+import { IconInfoCircle } from "@tabler/icons-react";
+
+export default function AlertIncomplete({ incompleteData, handleAlertClose }) {
+    return (
+        <Alert
+            mt="20px"
+            autoContrast
+            color="orange"
+            icon={<IconInfoCircle />}
+            onClose={handleAlertClose}
+            radius="xl"
+            title="Your profile is incomplete!"
+            withCloseButton={true}
+        >
+            <p>Please provide the following information:</p>
+            <ol>
+                {incompleteData?.map(({ label }) => (
+                    <li key={label}>{label}</li>
+                ))}
+            </ol>
+        </Alert>
+    );
+};
