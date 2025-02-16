2f89170d8d584858efab04c9463ddb16783d7105
Joseph Gordy <jgordy2424@gmail.com>
2025-02-15 23:00:51 -0500

    Remove gender and batting display from PersonalDetails component, simplifying the detail card layout.

--- app/routes/user/components/PersonalDetails.jsx
+++ app/routes/user/components/PersonalDetails.jsx
@@ -10,7 +10,6 @@ import {
 } from "@mantine/core";
 
 export default function DetailCard({ editButton, player, fieldsToDisplay }) {
-    const { gender, bats, ...rest } = player;
 
     return (
         <Card shadow="sm" padding="lg" radius="xl" withBorder>
@@ -23,30 +22,13 @@ export default function DetailCard({ editButton, player, fieldsToDisplay }) {
 
             <Divider my="xs" size="sm" />
 
-            <Group justify="space-around" gap="0px">
-                <Stack align="center" gap="0px">
-                    <Text size="xs">Gender</Text>
-                    <Text size="small" c="green" fw={700} autoContrast>{gender}</Text>
-                </Stack>
-
-
-                <Divider orientation="vertical" size="sm" />
-
-                <Stack align="center" gap="0px">
-                    <Text size="xs">Batting</Text>
-                    <Text size="small" c="green" fw={700} autoContrast>{bats}</Text>
-                </Stack>
-            </Group>
-
-            <Divider my="xs" size="sm" />
-
             <List
                 spacing="xs"
                 size="sm"
                 center
             >
                 {Object.entries(fieldsToDisplay).map(([key, { icon, label }]) => {
-                    const value = rest[key];
+                    const value = player[key];
                     return (
                         <List.Item key={key} icon={icon}>
                             <Text size="sm" c={!value ? 'red' : ''}>
