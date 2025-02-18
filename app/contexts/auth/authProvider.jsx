698cb292b8d67217461ebf23981db3406eff2ffc
Joseph Gordy <jgordy2424@gmail.com>
2025-02-17 23:29:20 -0500

    Add authentication context and provider for user management

---
+++ app/contexts/auth/authProvider.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+import { account } from '@/appwrite';
+
+import { readDocument } from '@/utils/databases';
+
+import AuthContext from './authContext';
+
+export default function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            try {
+                const currentUser = await account.get();
+                setUser(currentUser);
+            } catch (error) {
+                setUser(null); // No logged-in user
+            }
+        };
+
+        getLoggedInUser(); // Check on mount
+    }, []);
+
+    const getAndSetUser = async () => {
+        try {
+            const currentUser = await account.get();
+            setUser(currentUser);
+        } catch (error) {
+            console.log('Couldn\'t get user data');
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ user, setUser: getAndSetUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
