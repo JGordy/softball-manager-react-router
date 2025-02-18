dd7e9b070fce49e6745f6661c9f9c1d529920bea
Joseph Gordy <jgordy2424@gmail.com>
2025-02-17 23:27:59 -0500

    Add alias for contexts in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
             '@': '/app',
             '@components': '/app/components',
             '@constants': '/app/constants',
+            '@contexts': '/app/contexts',
             '@routes': '/app/routes',
             '@styles': '/app/styles',
             // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
