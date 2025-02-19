5299d814fac8cee64cb65d412832b5ef4e2b0eef
Joseph Gordy <jgordy2424@gmail.com>
2025-02-18 22:23:29 -0500

    Refactor routes in app/routes.js for improved organization and clarity

--- app/routes.js
+++ app/routes.js
@@ -13,17 +13,23 @@ export default [
         route('/verify', 'routes/auth/verify.jsx'),
         // route("/forgot-password", "routes/auth/recover.jsx"),
 
-
+        // The App shell (NavBar) in layout.jsx
         layout('routes/layout.jsx', [
+            // TODO: What to make this?
             index('routes/home/home.jsx'),
-
 
             // User routes
             route('/user/:userId', 'routes/user/profile.jsx'),
             route('/user/:userId/teams', 'routes/user/teams.jsx'),
+            // route('/user/:userId/events', 'routes/user/events.jsx'),
 
-            // Team routes
+
+            // Team routes TODO: With AuthContext implemented can we lose the "/user/:userId" part of the path?
             route('/user/:userId/teams/:teamId', 'routes/team/details.jsx'),
+            // TODO: Suggested
+            // route('/teams/:teamId', 'routes/team/details.jsx'),
+            // route('/teams/:teamId/seasons', 'routes/team/seasons.jsx'),
+            // route('/teams/:teamId/games', 'routes/team/games.jsx'),
 
             // Gameday Routes
             route('/gameday/lineup', 'routes/gameday/lineup.jsx'),
