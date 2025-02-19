6296c6624fbc6e757a4ee174a462f96600e95bea
Joseph Gordy <jgordy2424@gmail.com>
2025-02-19 14:11:55 -0500

    Add coachId retrieval in getTeamData for enhanced team data structure

--- app/routes/team/loader.js
+++ app/routes/team/loader.js
@@ -4,13 +4,18 @@ import { listDocuments, readDocument } from '@/utils/databases';
 export async function getTeamData({ teamId }) {
 
     if (teamId) {
+        // 1. Get memberships
         const memberships = await listDocuments('memberships', [
             Query.equal('teamId', teamId),
         ]);
 
-        // 2. Extract teamIds
+        // 2. Get the coach's id
+        const { userId: coachId } = memberships.documents.find(document => document.role === 'coach');
+
+        // 3. Extract teamIds
         const userIds = memberships.documents.map(m => m.userId);
 
+        // 4. Get all players
         let players = [];
         if (userIds.length > 0) {
             // Make multiple queries
@@ -28,6 +33,7 @@ export async function getTeamData({ teamId }) {
         return {
             teamData: await readDocument('teams', teamId),
             players,
+            coachId,
         };
     } else {
         return { teamData: {} };
