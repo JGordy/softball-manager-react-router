fce2312a2afcb3ca93a838586686870159b375b7
Joseph Gordy <jgordy2424@gmail.com>
2025-02-18 22:38:05 -0500

    Refactor positions constant to use an object structure with initials for improved clarity and usability

--- app/constants/positions.js
+++ app/constants/positions.js
@@ -1,14 +1,14 @@
-const positions = [
-    "Pitcher",
-    "Catcher",
-    "First Base",
-    "Second Base",
-    "Third Base",
-    "Shortstop",
-    "Left Field",
-    "Left Center Field",
-    "Right Center Field",
-    "Right Field",
-];
+const positions = {
+    Pitcher: { initials: 'P' },
+    Catcher: { initials: 'C' },
+    'First Base': { initials: '1B' },
+    'Second Base': { initials: '2B' },
+    'Third Base': { initials: '3B' },
+    'Shortstop': { initials: 'SS' },
+    'Left Field': { initials: 'LF' },
+    'Left Center Field': { initials: 'LC' },
+    'Right Center Field': { initials: 'RC' },
+    'Right Field': { initials: 'RF' },
+};
 
 export default positions;
