4d7c6092cbda4e13360c35a2b549d77fd18484a4
Joseph Gordy <jgordy2424@gmail.com>
2025-02-19 14:46:40 -0500

    Enhance TeamDetails component to conditionally display player addition button for coaches

--- app/routes/team/details.jsx
+++ app/routes/team/details.jsx
@@ -19,6 +19,8 @@ import { IconCurrencyDollar, IconCalendar, IconMapPin, IconFriends, IconCalendar
 
 import { adjustColorBasedOnDarkness } from '@/utils/adjustHexColor';
 
+import { useAuth } from '@/contexts/auth/useAuth';
+
 import PlayerForm from './components/PlayerForm';
 import PlayerList from './components/PlayerList';
 
@@ -35,7 +37,11 @@ export async function loader({ params }) {
 };
 
 export default function TeamDetails({ actionData, loaderData }) {
-    const { teamData, players } = loaderData;
+    const { teamData, players, coachId } = loaderData;
+
+    const { user } = useAuth();
+
+    const coachView = coachId === user?.$id;
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [teamDetails, setTeamDetails] = useState(teamData || {});
@@ -45,8 +51,6 @@ export default function TeamDetails({ actionData, loaderData }) {
         const handleAfterSubmit = async () => {
             try {
                 if (actionData && actionData.status === 200) {
-                    // const { response } = actionData;
-
                     setError(null);
                     setIsModalOpen(false);
                 } else if (actionData instanceof Error) {
@@ -134,16 +138,19 @@ export default function TeamDetails({ actionData, loaderData }) {
             </Stack>
 
             <Card mt="lg" radius="md" padding="xs">
-                <PlayerList players={players} />
+                <PlayerList players={players} coachId={coachId} coachView={coachView} />
 
-                <Button
-                    mt="md"
-                    color={primaryColor}
-                    onClick={() => setIsModalOpen(true)}
-                    autoContrast
-                >
-                    Add Player
-                </Button>
+                {coachView && (
+
+                    <Button
+                        mt="md"
+                        color={primaryColor}
+                        onClick={() => setIsModalOpen(true)}
+                        autoContrast
+                    >
+                        Add Player
+                    </Button>
+                )}
             </Card>
 
             <Modal opened={isModalOpen} onClose={() => setIsModalOpen(false)} title="Add a Player">
