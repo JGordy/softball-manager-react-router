acc2f8524b6291b3d6e81a5bee2ddada5941042c
Joseph Gordy <jgordy2424@gmail.com>
2025-02-20 16:21:39 -0500

    Add EditButton component for opening edit modal

---
+++ app/components/EditButton.jsx
@@ -0,0 +1,24 @@
+import { Button, Group, Text, useComputedColorScheme } from '@mantine/core';
+
+import { IconEdit } from '@tabler/icons-react';
+
+export default function EditButton({ setIsModalOpen }) {
+    const computedColorScheme = useComputedColorScheme('light');
+
+    return (
+        <Button
+            variant="subtle"
+            color={computedColorScheme === 'light' ? 'black' : 'white'}
+            onClick={() => setIsModalOpen(true)}
+            p="0"
+            autoContrast
+        >
+            <Group gap="5px">
+                <IconEdit size={18} />
+                <Text size="sm">
+                    Edit
+                </Text>
+            </Group>
+        </Button>
+    );
+}
