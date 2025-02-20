5eabb62f75c1fa5b9d9ecaeb462474ab6ac23592
Joseph Gordy <jgordy2424@gmail.com>
2025-02-19 21:28:44 -0500

    Add SeasonForm component for managing season details and submissions

---
+++ app/routes/team/components/SeasonForm.jsx
@@ -0,0 +1,98 @@
+import { Form } from 'react-router';
+
+import {
+    Button,
+    Group,
+    MultiSelect,
+    Select,
+    TextInput,
+} from '@mantine/core';
+
+import { DatePickerInput } from '@mantine/dates';
+
+import { IconCalendar } from '@tabler/icons-react';
+
+import classes from '@/styles/inputs.module.css';
+
+export default function SeasonForm({ handleCloseModal, setError, primaryColor, teamId }) {
+
+    const iconProps = {
+        color: 'currentColor',
+        size: 18,
+        stroke: 1.5
+    };
+
+    return (
+        <Form method="post">
+            <input type="hidden" name="teamId" value={teamId} />
+            <TextInput
+                className={classes.inputs}
+                label="Season Name"
+                name="seasonName"
+                placeholder='Fall Season 2025'
+                required
+            />
+            <TextInput
+                className={classes.inputs}
+                label="Location"
+                name="location"
+                placeholder='Where will the games be played?'
+            />
+            <MultiSelect
+                className={classes.inputs}
+                label="Game Days"
+                name="gameDays"
+                placeholder="What day(s) are games played?"
+                data={['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']}
+                mb="sm"
+                clearable
+                searchable
+            />
+            <Select
+                className={classes.inputs}
+                label="League Type"
+                name="leagueType"
+                placeholder="Select Gender"
+                data={['Men', 'Women', 'Coed']}
+                mb="sm"
+                required
+            />
+            <DatePickerInput
+                className={classes.inputs}
+                leftSection={<IconCalendar {...iconProps} />}
+                label="Season Start Date"
+                name="seasonStartDate"
+                placeholder="Pick a date"
+            />
+            <DatePickerInput
+                className={classes.inputs}
+                leftSection={<IconCalendar {...iconProps} />}
+                label="Season End Date"
+                name="seasonEndDate"
+                placeholder="Pick a date"
+            />
+
+            <Group position="right" mt="lg">
+                <Button
+                    type="submit"
+                    color={primaryColor}
+                    name="_action"
+                    value="add-season"
+                    autoContrast
+                >
+                    Submit
+                </Button>
+                <Button
+                    variant="outline"
+                    color="gray"
+                    onClick={() => {
+                        handleCloseModal();
+                        setError(null);
+                    }}
+                >
+                    Cancel
+                </Button>
+            </Group>
+        </Form>
+    );
+};
