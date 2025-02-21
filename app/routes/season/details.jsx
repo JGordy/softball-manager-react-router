6c69cb9b3a77fd20aba3e97e53b4d1fca3ff96bf
Joseph Gordy <jgordy2424@gmail.com>
2025-02-20 23:00:28 -0500

    Enhance SeasonDetails component with edit functionality and improved layout

--- app/routes/season/details.jsx
+++ app/routes/season/details.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
+import { Container, Group, Title } from '@mantine/core';
+
 import BackButton from '@/components/BackButton';
+import EditButton from '@/components/EditButton';
 import { getSeasonDetails } from './loader';
 
 export async function loader({ params }) {
@@ -11,12 +16,22 @@ export default function SeasonDetails({ loaderData }) {
     const { season } = loaderData;
     console.log('/season/details.jsx: ', { season });
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleEditSeasonDetails = () => {
+        setIsModalOpen(true)
+    };
+
     return (
-        <div>
-            <BackButton />
-            <h1>Season Details</h1>
-            <p>{season.seasonName}</p>
-            <p>{season.games.length} games</p>
-        </div>
+        <Container size="xl" p="xl">
+            <Group justify="space-between">
+                <BackButton text="Teams" />
+                <EditButton setIsModalOpen={handleEditSeasonDetails} />
+            </Group>
+            <Title order={2} align="center" mt="sm" mb="lg">
+                {season.seasonName}
+            </Title>
+            <p>{season.games.length} games listed</p>
+        </Container>
     );
 }
