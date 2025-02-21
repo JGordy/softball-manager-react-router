87b26e40e1b29caf6ce24919a5d7d2c80f0cc0d5
Joseph Gordy <jgordy2424@gmail.com>
2025-02-21 15:34:08 -0500

    Refactor game creation logic to use createDocument and handle multiple games

--- app/routes/season/action.js
+++ app/routes/season/action.js
@@ -1,22 +1,23 @@
-import { updateDocument } from '@/utils/databases';
+import { ID } from '@/appwrite';
+import { createDocument, updateDocument } from '@/utils/databases';
 
-export async function addGames({ values, teamId }) {
-    // Removes undefined or empty string values from data to update
-    // const dataToUpdate = {};
-    // for (const key in values) {
-    //     if (values.hasOwnProperty(key) && values[key] !== undefined && values[key] !== "") {
-    //         dataToUpdate[key] = values[key];
-    //     }
-    // }
+export async function createGames({ values }) {
+    const { games: generatedGames } = values;
+    let games = JSON.parse(generatedGames);
 
-    // try {
-    //     const teamDetails = await updateDocument('teams', teamId, dataToUpdate);
+    try {
+        const createdGames = [];
 
-    //     return { response: { teamDetails }, status: 204, success: true }
-    // } catch (error) {
-    //     console.error("Error updating team:", error);
-    //     throw error;
-    // }
+        for (const game of games) {
+            const createdGame = await createDocument('games', ID.unique(), game);
+            createdGames.push(createdGame);
+        }
+
+        return { response: { games: createdGames }, status: 201, success: true };
+    } catch (error) {
+        console.error("Error creating games:", error);
+        throw error;
+    }
 }
 
 export async function updateSeason({ values, seasonId }) {
