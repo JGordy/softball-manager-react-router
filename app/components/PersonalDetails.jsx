6fdc300248c8439ad3e2dc7bbec43cd2bb0804a6
Joseph Gordy <jgordy2424@gmail.com>
2025-02-24 15:20:04 -0500

    Add PersonalDetails and PlayerDetails components for enhanced player information display

---
+++ app/components/PersonalDetails.jsx
@@ -0,0 +1,67 @@
+import {
+    Card,
+    Divider,
+    Group,
+    List,
+    Text,
+    Title,
+} from "@mantine/core";
+
+import {
+    IconFriends,
+    IconHeadphonesFilled,
+    IconMail,
+    IconPhone,
+} from '@tabler/icons-react';
+
+const fields = {
+    email: {
+        icon: <IconMail size={20} />,
+        label: 'email',
+    },
+    phoneNumber: {
+        icon: <IconPhone size={20} />,
+        label: 'phone number',
+    },
+    gender: {
+        icon: <IconFriends size={20} />,
+        label: 'gender',
+    },
+    walkUpSong: {
+        icon: <IconHeadphonesFilled size={20} />,
+        label: 'walk up song',
+    },
+};
+
+export default function DetailCard({ editButton, player, fieldsToDisplay }) {
+
+    return (
+        <Card shadow="sm" padding="lg" radius="xl" withBorder>
+            <Group justify="space-between">
+                <Group>
+                    <Title order={4}>Personal Details</Title>
+                </Group>
+                {editButton}
+            </Group>
+
+            <Divider my="xs" size="sm" />
+
+            <List
+                spacing="xs"
+                size="sm"
+                center
+            >
+                {Object.entries({ ...fields, ...fieldsToDisplay }).map(([key, { icon, label }]) => {
+                    const value = player[key];
+                    return (
+                        <List.Item key={key} icon={icon}>
+                            <Text size="sm" c={!value ? 'red' : ''}>
+                                {value || `${label} not listed*`}
+                            </Text>
+                        </List.Item>
+                    );
+                })}
+            </List>
+        </Card>
+    );
+};
---
+++ app/components/PlayerDetails.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import {
+    Avatar,
+    Card,
+    ColorSwatch,
+    Divider,
+    Group,
+    Image,
+    Text,
+    Title,
+} from '@mantine/core';
+
+import fieldPositions from '@/constants/positions';
+
+import styles from '@/styles/positionChart.module.css';
+
+const fieldSrc = `${import.meta.env.VITE_APPWRITE_HOST_URL}/storage/buckets/67af948b00375c741493/files/67b00f90002a66960ba4/view?project=${import.meta.env.VITE_APPWRITE_PROJECT_ID}&mode=admin`;
+
+const colors = {
+    PREFERRED: 'rgba(0, 249, 50, 0.5)',
+    NEUTRAL: 'rgba(208, 210, 209, 0.5)',
+    DISLIKED: 'rgba(249, 0, 0, 0.5)',
+};
+
+function FieldPosition({ position, initials, isPreferred, isDisliked }) {
+    let color = colors.NEUTRAL;
+
+    if (isDisliked) color = colors.DISLIKED;
+    if (isPreferred) color = colors.PREFERRED;
+
+    return (
+        <div
+            className={`${styles.fieldingPosition} ${styles[position.toLowerCase().replace(/\s+/g, '')]}`}
+        >
+            <Avatar
+                size="sm"
+                name={initials}
+                alt={position}
+                variant="filled"
+                color={color}
+                autoContrast
+            />
+        </div>
+    );
+}
+
+function PlayerDetails({ player, editButton }) {
+    const { throws, bats, preferredPositions, dislikedPositions } = player;
+
+    return (
+        <Card shadow="sm" py="lg" px="xs" radius="xl" mt="md" withBorder>
+            <Group justify="space-between" px="10px">
+                <Title order={4}>Player Details</Title>
+                {editButton}
+            </Group>
+
+            <Divider my="sm" size="sm" />
+
+            <Group gap="xl" px="xs">
+                <Group gap="4px">
+                    <Text>Throws</Text>
+                    <Text fw={700} c={throws ? 'green' : 'red'}>{throws || "Not Specified"}</Text>
+                </Group>
+                <Group gap="4px">
+                    <Text>Bats</Text>
+                    <Text fw={700} c={bats ? 'green' : 'red'}>{bats || "Not Specified"}</Text>
+                </Group>
+            </Group>
+
+            <Divider my="sm" size="sm" />
+
+            <Text pl="10px">Fielding Preferences</Text>
+
+            <div className={styles.imageContainer}>
+                <Image src={fieldSrc} alt="Preferred Positions Chart" />
+
+                {Object.keys(fieldPositions).map((position) => (
+                    <FieldPosition
+                        key={position}
+                        position={position}
+                        isPreferred={preferredPositions.includes(position)}
+                        isDisliked={dislikedPositions.includes(position)}
+                        {...fieldPositions[position]}
+                    />
+                ))}
+            </div>
+            <Group justify='space-between' px="10px">
+                <Group gap="xs">
+                    <ColorSwatch color={colors.PREFERRED} />
+                    Preferred
+                </Group>
+                <Group gap="xs">
+                    <ColorSwatch color={colors.NEUTRAL} />
+                    Open
+                </Group>
+                <Group gap="xs">
+                    <ColorSwatch color={colors.DISLIKED} />
+                    Disliked
+                </Group>
+            </Group>
+        </Card>
+    );
+}
+
+export default PlayerDetails;
