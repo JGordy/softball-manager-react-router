3e40871af0a6b8f745b1d8fe213f419da9ffdfc4
Joseph Gordy <jgordy2424@gmail.com>
2025-02-24 12:15:04 -0500

    Add North American time zones to constants for improved time management

---
+++ app/constants/timeZones.js
@@ -0,0 +1,34 @@
+const northAmericanTimeZones = [
+    'UTC',
+    'America/New_York',
+    'America/Chicago',
+    'America/Denver',
+    'America/Los_Angeles',
+    'America/Phoenix', // Arizona (no DST)
+    'America/Anchorage',
+    'America/Honolulu',
+    'America/Puerto_Rico',
+    'America/Vancouver',
+    'America/Edmonton',
+    'America/Mexico_City',
+    'America/Monterrey',
+    'America/Tijuana',
+    'America/Regina', // Saskatchewan, Canada (no DST)
+    'America/Managua',
+    'America/Panama',
+    'America/Santo_Domingo',
+    'America/Halifax',
+    'America/St_Johns',
+    'America/Nassau',
+    'America/Barbados',
+    'America/Jamaica',
+    'America/Guatemala',
+    'America/El_Salvador',
+    'America/Costa_Rica',
+    'America/Belize',
+    'America/Cayman',
+];
+
+export default [
+    ...northAmericanTimeZones,
+];
