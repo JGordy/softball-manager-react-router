1c0bef64818eb14088d456ad7c8e686de14567e2
Joseph Gordy <jgordy2424@gmail.com>
2025-02-23 23:11:33 -0500

    Replace lineup generation icon with a sparkles icon for improved visual appeal

--- app/routes/gameday/lineup.jsx
+++ app/routes/gameday/lineup.jsx
@@ -10,7 +10,7 @@ import {
 
 import PlayerChart from '@components/PlayerChart';
 
-import { IconWand, IconPrinter } from '@tabler/icons-react';
+import { IconWand, IconPrinter, IconSparkles } from '@tabler/icons-react';
 
 import createBattingOrder from './utils/createBattingOrder';
 import createFieldingChart from './utils/createFieldingChart';
@@ -119,7 +119,7 @@ function Lineup({ loaderData, actionData }) {
                         loading={isLoading}
                         onClick={handleGenerateLineup}
                     >
-                        <IconWand size={18} />
+                        <IconSparkles size={18} />
                         Generate Lineup
                     </Button>
                 </Group>
