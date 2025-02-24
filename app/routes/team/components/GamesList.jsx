1ed1a120a0a6dbdfdefeafb406d51c1dea076bbc
Joseph Gordy <jgordy2424@gmail.com>
2025-02-24 12:16:41 -0500

    (WIP) Refactor GamesList component to display games in cards and format game time using dateTime utility

--- app/routes/team/components/GamesList.jsx
+++ app/routes/team/components/GamesList.jsx
@@ -1,4 +1,6 @@
-import { Text } from '@mantine/core';
+import { Card, Group, Text } from '@mantine/core';
+
+import { formatGameTime } from '@/utils/dateTime';
 
 export default function GamesList({ games, primaryColor }) {
 
@@ -10,9 +12,24 @@ export default function GamesList({ games, primaryColor }) {
         );
     }
 
+    const gameList = games.map(game => (
+        <Card key={game.$id} mt="sm" radius="md" padding="sm" withBorder>
+            <Group justify="space-between">
+                {/* <Text>{season.seasonName}</Text> */}
+                <Group spacing="xs">
+                    <span>
+                        {formatGameTime(game.gameDate, game.timeZone)}
+                    </span>
+                </Group>
+            </Group>
+
+            {/* {getSeasonStatus(season)} */}
+        </Card>
+    ));
+
     return (
-        <Text>
-            Games List
-        </Text>
+        <>
+            {gameList}
+        </>
     );
 };
