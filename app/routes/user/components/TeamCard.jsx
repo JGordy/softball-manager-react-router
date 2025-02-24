910822e2fe21a248be97f1b706743b5b686dea44
Joseph Gordy <jgordy2424@gmail.com>
2025-02-23 22:02:46 -0500

    Enhance TeamCard component to display season status and improve layout

--- app/routes/user/components/TeamCard.jsx
+++ app/routes/user/components/TeamCard.jsx
@@ -12,20 +12,57 @@ import {
 import {
     IconCalendar,
     IconFriends,
-    IconMapPin,
 } from '@tabler/icons-react';
 
 import { adjustColorBasedOnDarkness } from '@/utils/adjustHexColor';
 
-export default function TeamCard({ team, userId }) {
+export default function TeamCard({ team }) {
 
     const computedColorScheme = useComputedColorScheme('light');
 
     const { primaryColor } = team;
     const adjustedColor = adjustColorBasedOnDarkness(primaryColor, computedColorScheme === 'light' ? 100 : 50);
 
-    const season = team.seasons?.[0];
-    const latestSeasonDates = season?.startDate ? `${new Date(season.startDate).toLocaleDateString()} - ${new Date(season.endDate).toLocaleDateString()}` : 'No season data listed';
+    const getSeasonStatus = () => {
+        const { seasons } = team;
+        if (!seasons || seasons.length === 0) {
+            return null; // No seasons, no status
+        }
+
+        const today = new Date();
+        const oneMonthFromNow = new Date(today);
+        oneMonthFromNow.setMonth(today.getMonth() + 1);
+
+        for (const season of seasons) {
+            const startDate = new Date(season.startDate);
+            const endDate = new Date(season.endDate);
+
+            if (startDate <= today && today <= endDate) {
+                return 'Season in progress';
+            }
+
+            if (startDate > today && startDate <= oneMonthFromNow) {
+                const timeDiff = startDate.getTime() - today.getTime();
+                const daysUntilStart = Math.ceil(timeDiff / (1000 * 3600 * 24)); // Calculate days
+
+                const daysUntilText = `${daysUntilStart} day${daysUntilStart !== 1 ? 's' : ''}`;
+
+                const daysUntil = (
+                    <Text span fw={700} c="green">
+                        {daysUntilText}
+                    </Text>
+                );
+
+                return (
+                    <>
+                        Season starts in {daysUntil}
+                    </>
+                );
+            }
+        }
+
+        return 'No upcoming seasons';
+    }
 
     const iconProps = {
         variant: 'gradient',
@@ -61,25 +98,19 @@ export default function TeamCard({ team, userId }) {
                             <IconCalendar size={16} />
                         </ThemeIcon>
                         <Text size="md">
-                            {latestSeasonDates}
+                            {getSeasonStatus()}
                         </Text>
                     </Group>
-                    <Group gap="xs">
-                        <ThemeIcon {...iconProps}>
-                            <IconFriends size={18} />
-                        </ThemeIcon>
-                        <Text size="md">
-                            {team.genderMix || 'Not Specified'}
-                        </Text>
-                    </Group>
-                    <Group gap="xs">
-                        <ThemeIcon {...iconProps}>
-                            <IconMapPin size={16} />
-                        </ThemeIcon>
-                        <Text size="md">
-                            {season?.location || "Location not specified"}
-                        </Text>
-                    </Group>
+                    {team.genderMix && (
+                        <Group gap="xs">
+                            <ThemeIcon {...iconProps}>
+                                <IconFriends size={18} />
+                            </ThemeIcon>
+                            <Text size="md">
+                                {team.genderMix}
+                            </Text>
+                        </Group>
+                    )}
                 </Group>
             </Card>
         </Link>
