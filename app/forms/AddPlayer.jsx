f9f1205bd74180858d639b4fc213df43c1bfad41
Joseph Gordy <jgordy2424@gmail.com>
2025-02-26 16:02:31 -0500

    Refactor AddPlayer component to conditionally display input fields based on props

--- app/forms/AddPlayer.jsx
+++ app/forms/AddPlayer.jsx
@@ -19,75 +19,116 @@ export default function AddPlayer({
     action = 'add-player',
     actionRoute,
     buttonColor,
+    confirmText = 'Create Player',
+    inputsToDisplay = [],
 }) {
+
+    const shouldDisplay = (field) => inputsToDisplay.includes(field);
 
     return (
         <FormWrapper
             action={action}
             actionRoute={actionRoute}
             buttonColor={buttonColor}
-            confirmText="Create Player"
+            confirmText={confirmText}
         >
-            <TextInput
-                className={classes.inputs}
-                label="First Name"
-                name="firstName"
-                required
-            />
-            <TextInput
-                className={classes.inputs}
-                label="Last Name"
-                name="lastName"
-                required
-            />
-            <AutocompleteEmail classes={classes.inputs} required />
-            <Input.Wrapper className={classes.inputs}>
-                <Input.Label>Phone Number</Input.Label>
-                <Input
-                    component={IMaskInput}
-                    label="Phone Number"
-                    mask="(000) 000-0000"
-                    name="phoneNumber"
-                    placeholder='(xxx) xxx-xxxx'
+            {shouldDisplay('name') && (
+                <>
+                    <TextInput
+                        className={classes.inputs}
+                        label="First Name"
+                        name="firstName"
+                        required={action === 'add-player'}
+                    />
+                    <TextInput
+                        className={classes.inputs}
+                        label="Last Name"
+                        name="lastName"
+                        required={action === 'add-player'}
+                    />
+                </>
+            )}
+            {shouldDisplay('contact') && (
+                <>
+                    <AutocompleteEmail classes={classes.inputs} required={action === 'add-player'} />
+                    <Input.Wrapper className={classes.inputs}>
+                        <Input.Label>Phone Number</Input.Label>
+                        <Input
+                            component={IMaskInput}
+                            label="Phone Number"
+                            mask="(000) 000-0000"
+                            name="phoneNumber"
+                            placeholder='(xxx) xxx-xxxx'
+                        />
+                    </Input.Wrapper>
+                </>
+            )}
+            {shouldDisplay('gender') && (
+                <Select
+                    className={classes.inputs}
+                    label="Gender"
+                    name="gender"
+                    placeholder="Select Gender"
+                    data={['Male', 'Female']}
+                    mb="sm"
+                    required={action === 'add-player'}
                 />
-            </Input.Wrapper>
-            <Select
-                className={classes.inputs}
-                label="Gender"
-                name="gender"
-                placeholder="Select Gender"
-                data={['Male', 'Female']}
-                mb="sm"
-                required
-            />
-            <MultiSelect
-                className={classes.inputs}
-                label="Preferred Positions"
-                name="preferredPositions"
-                placeholder="Select Positions"
-                data={Object.keys(positions)}
-                mb="sm"
-                clearable
-                searchable
-                required
-            />
-            <MultiSelect
-                className={classes.inputs}
-                label="Disliked Positions"
-                name="dislikedPositions"
-                placeholder="What positions are you NOT interested in?"
-                data={Object.keys(positions)}
-                mb="sm"
-                clearable
-                searchable
-            />
-            <TextInput
-                className={classes.inputs}
-                label="Walk Up Song"
-                name="walkUpSong"
-                placeholder="Walk Up Song"
-                mb="sm"
-            />
+            )}
+            {shouldDisplay('throws-bats') && (
+                <>
+                    <Select
+                        className={classes.inputs}
+                        label="Throws"
+                        name="throws"
+                        placeholder="Which hand do you throw with?"
+                        data={['Right', 'Left']}
+                        mb="sm"
+                    />
+                    <Select
+                        className={classes.inputs}
+                        label="Bats"
+                        name="bats"
+                        placeholder="On what side of the plate do you bat?"
+                        data={['Right', 'Left', 'Switch']}
+                        mb="sm"
+                    />
+                </>
+            )}
+
+            {shouldDisplay('positions') && (
+                <>
+                    <MultiSelect
+                        className={classes.inputs}
+                        label="Preferred Positions"
+                        name="preferredPositions"
+                        placeholder="What positions do you prefer?"
+                        data={Object.keys(positions)}
+                        mb="sm"
+                        clearable
+                        searchable
+                        required={action === 'add-player'}
+                    />
+                    <MultiSelect
+                        className={classes.inputs}
+                        label="Disliked Positions"
+                        name="dislikedPositions"
+                        placeholder="What positions are you NOT interested in?"
+                        data={Object.keys(positions)}
+                        mb="sm"
+                        clearable
+                        searchable
+                    />
+                </>
+            )}
+            {shouldDisplay('song') && (
+                <TextInput
+                    className={classes.inputs}
+                    label="Walk Up Song"
+                    name="walkUpSong"
+                    placeholder="Walk Up Song"
+                    mb="sm"
+                />
+            )}
         </FormWrapper>
     );
 };
