d8889834c576444f6505d88a249008bc4e0b2ad3
Joseph Gordy <jgordy2424@gmail.com>
2025-02-27 13:23:51 -0500

    Add UserHeader component for personalized user greeting

---
+++ app/components/UserHeader.jsx
@@ -0,0 +1,15 @@
+import { Avatar, Group, Text, Title } from '@mantine/core';
+
+export default function UserHeader({ user, subText }) {
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+    return (
+        <Group my="md">
+            <Avatar color="green" name={fullName} alt={fullName} size="md" />
+            <div>
+                <Title order={3}>{`Hello ${user.firstName}!`}</Title>
+                {subText && <Text size="0.6rem">{subText}</Text>}
+            </div>
+        </Group>
+    );
+}
