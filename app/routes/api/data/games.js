9b133372c7ae9d42eedf87df183991d1c5f0402b
Joseph Gordy <jgordy2424@gmail.com>
2025-02-27 14:23:05 -0500

    Add API route to fetch user games based on team memberships

---
+++ app/routes/api/data/games.js
@@ -0,0 +1,50 @@
+import { Query } from '@/appwrite';
+import { listDocuments, readDocument } from '@/utils/databases';
+
+import getGames from '@/utils/getGames';
+
+export async function action({ request, params }) {
+    const { userId } = await request.json();
+
+    if (!userId) {
+        return { user: {}, teams: [], games: [] };
+    }
+
+    try {
+        // 1. Get user profile data
+        const user = await readDocument('users', userId);
+
+        // 2. Check relationships table to list memberships for the userId, both manager and player
+        const memberships = await listDocuments('memberships', [
+            Query.equal('userId', userId),
+            Query.equal('role', ['manager', 'player']),
+        ]);
+
+        // 4. Fetch teams for managers and players
+        const fetchTeams = async (teamIds) => {
+            if (teamIds.length === 0) {
+                return [];
+            }
+
+            const promises = teamIds.map(async (teamId) => {
+                const result = await listDocuments('teams', [
+                    Query.equal('$id', teamId),
+                ]);
+                return result.documents;
+            });
+
+            const results = await Promise.all(promises);
+            return results.flat();
+        };
+
+        const teams = await fetchTeams(memberships);
+
+        const games = getGames({ teams, futureOnly: true });
+
+        return { user, games };
+
+    } catch (error) {
+        console.error('Error getting teams: ', error);
+        throw error;
+    }
+}
