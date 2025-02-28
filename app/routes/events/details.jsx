9ec7d7ada42d4544e30f5e3d19cb70440dac8187
Joseph Gordy <jgordy2424@gmail.com>
2025-02-28 15:59:22 -0500

    Enhance EventsDetails component with filtering options for game history and improved UI elements

--- app/routes/events/details.jsx
+++ app/routes/events/details.jsx
@@ -1,11 +1,26 @@
+import { useState, useRef } from 'react';
 import { redirect, useOutletContext } from 'react-router';
 
-import { Container } from "@mantine/core";
+import {
+    ActionIcon,
+    Container,
+    Group,
+    Menu,
+    Tabs,
+    Text,
+    Title,
+    useComputedColorScheme,
+} from "@mantine/core";
+
+import { IconAdjustments, IconCalendarMonth } from '@tabler/icons-react';
 
 import { account } from '@/appwrite';
 
 import LoaderDots from '@/components/LoaderDots';
 import UserHeader from '@/components/UserHeader';
+import GamesList from '@/components/GamesList';
+
+import getGames from '@/utils/getGames';
 
 export async function clientLoader({ request }) {
     try {
@@ -39,18 +54,93 @@ export async function clientLoader({ request }) {
 clientLoader.hydrate = true;
 
 export function HydrateFallback() {
-    return <LoaderDots message="Fetching your teams..." />;
+    return <LoaderDots message="Fetching your scheduled events..." />;
 }
 
 export default function EventsDetails({ loaderData }) {
     const teams = loaderData?.teams;
     const { user } = useOutletContext();
-    console.log('/events ', { user, teams });
+
+    const computedColorScheme = useComputedColorScheme('light');
+
+    const ref = useRef();
+
+    const [filterId, setFilterId] = useState();
+    const [showFilters, setShowFilters] = useState(false);
+
+    const teamsData = [...teams?.managing, ...teams?.playing];
+
+    const { futureGames, pastGames } = getGames({ teams: teamsData });
+    console.log('/events ', { user, teams, futureGames, pastGames });
+
+    const handleMenuItemClick = (teamId) => {
+        console.log('/event > handleMenuItemClick', { teamId });
+        setFilterId(teamId);
+        setShowFilters(false); // Close the menu after clicking an item
+    };
+
+    const toggleMenu = () => {
+        setShowFilters((prev) => !prev);
+    };
+
+    const handleMenuClose = () => {
+        setShowFilters(false);
+    };
 
     return (
         <Container p="md" mih="90vh">
-            <UserHeader subText="Here are all of your events" />
-            <div>Events list</div>
+            <Group justify="space-between">
+                <UserHeader subText="Track your game history" />
+
+                <Menu
+                    width="90vw"
+                    position="bottom-end"
+                    offset={0}
+                    opened={showFilters}
+                    onClose={handleMenuClose}
+                    trigger="click"
+                >
+                    <Menu.Target>
+                        <ActionIcon variant="default" radius="xl" aria-label="Filter Games" size="lg" onClick={toggleMenu}>
+                            <IconAdjustments stroke={1.5} size={24} />
+                        </ActionIcon>
+                    </Menu.Target>
+                    <Menu.Dropdown bg={(computedColorScheme === 'light') ? "gray.1" : "#1a242f"}>
+                        <Menu.Label>Filter Games by Team</Menu.Label>
+
+                        {teamsData?.map(team => (
+                            <Menu.Item
+                                key={team.name}
+                                onClick={() => handleMenuItemClick(team.$id)}
+                                bg={filterId === team.$id ? team.primaryColor : undefined}
+                                mt="xs"
+                            >
+                                <Text c={filterId === team.$id ? 'white' : undefined}>{team.name}</Text>
+                            </Menu.Item>
+                        ))}
+                    </Menu.Dropdown>
+                </Menu>
+            </Group>
+
+            <Title order={5} mt="lg">See detailed information for upcoming and past games</Title>
+            <Tabs radius="md" defaultValue="upcoming" mt="xl">
+                <Tabs.List grow justify="center">
+                    <Tabs.Tab value="upcoming" size="lg" leftSection={<IconCalendarMonth size={16} />}>
+                        Upcoming
+                    </Tabs.Tab>
+                    <Tabs.Tab value="past" size="lg" leftSection={<IconCalendarMonth size={16} />}>
+                        Past
+                    </Tabs.Tab>
+                </Tabs.List>
+
+                <Tabs.Panel value="upcoming">
+                    <GamesList games={futureGames.filter(game => game.teamId === filterId)} height="65vh" />
+                </Tabs.Panel>
+
+                <Tabs.Panel value="past">
+                    <GamesList games={pastGames.filter(game => game.teamId === filterId)} height="65vh" />
+                </Tabs.Panel>
+            </Tabs>
         </Container>
     );
 };
