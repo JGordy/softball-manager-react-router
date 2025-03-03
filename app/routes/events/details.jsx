3a71309c2d2744379b08fda38139e8170c0f435c
Joseph Gordy <jgordy2424@gmail.com>
2025-03-03 11:40:01 -0500

    Enhance EventDetails component with game details display and back navigation

--- app/routes/events/details.jsx
+++ app/routes/events/details.jsx
@@ -1,10 +1,39 @@
-import { Container } from '@mantine/core';
+import { Container, Flex, Group, Text, Title } from '@mantine/core';
+
+import BackButton from '@/components/BackButton';
+
+import { formatGameTime } from '@/utils/dateTime';
+
+import { getEventDetails } from './loader';
+
+export async function loader({ params }) {
+    const { eventId } = params;
+
+    return { game: await getEventDetails({ eventId }) }
+}
 
 export default function EventDetails({ loaderData }) {
     console.log('/events/:eventId > ', { loaderData });
+    const { game } = loaderData;
+    const { opponent, gameDate, isHomeGame, timeZone } = game;
+    const team = game?.seasons?.teams[0]?.name;
+
+    const formattedGameTime = formatGameTime(gameDate, timeZone);
+
     return (
-        <Container>
-            Event Details
+        <Container p="md" mih="90vh">
+            <Group justify="space=between">
+                <BackButton />
+            </Group>
+            {Object.keys(game) && (
+                <Flex mt="lg" direction="column">
+                    <Title order={4}>
+                        {team} {isHomeGame ? 'vs' : ''} {opponent}
+                    </Title>
+
+                    <Text>{formattedGameTime}</Text>
+                </Flex>
+            )}
         </Container>
     );
 }
