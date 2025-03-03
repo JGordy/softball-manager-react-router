09c5226f90d9776cbc22693b7845f7f660ea43e6
Joseph Gordy <jgordy2424@gmail.com>
2025-03-03 16:25:34 -0500

    Add updateGame function to handle game updates and clean input data

---
+++ app/routes/events/action.js
@@ -0,0 +1,32 @@
+import { updateDocument } from '@/utils/databases';
+import { combineDateTime } from '@/utils/dateTime';
+
+export async function updateGame({ values, eventId }) {
+    // Removes undefined or empty string values from data to update
+    console.log({ values });
+    let dataToUpdate = {};
+    for (const key in values) {
+        if (values.hasOwnProperty(key) && values[key] !== undefined && values[key] !== "") {
+            dataToUpdate[key] = values[key];
+        }
+    }
+
+    if (values.gameDate && values.gameTime) {
+        dataToUpdate.gameTime = combineDateTime(values.gameDate, values.gameTime);
+    }
+
+    if (values.isHomeGame) {
+        dataToUpdate.isHomeGame = values.isHomeGame === 'true';
+    }
+
+    delete dataToUpdate.gameTime;
+
+    try {
+        const gameDetails = await updateDocument('games', eventId, dataToUpdate);
+
+        return { response: { gameDetails }, status: 204, success: true };
+    } catch (error) {
+        console.error("Error updating game:", error);
+        throw error;
+    }
+}
