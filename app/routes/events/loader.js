d12f74c0fc96b0f4ddd3c33233ea7ef40bde00f4
Joseph Gordy <jgordy2424@gmail.com>
2025-03-03 14:52:21 -0500

    Fetch detailed event information including teams and players from the database

--- app/routes/events/loader.js
+++ app/routes/events/loader.js
@@ -1,5 +1,21 @@
-import { readDocument } from '@/utils/databases';
+import { Query } from '@/appwrite';
+import { listDocuments, readDocument } from '@/utils/databases';
 
 export async function getEventDetails({ eventId }) {
-    return await readDocument('games', eventId);
+    const { seasons: season, ...game } = await readDocument('games', eventId);
+    const { teams = [] } = season;
+    const { documents: userIds } = await await listDocuments('memberships', [
+        Query.equal('teamId', [teams[0].$id]),
+    ]);
+
+    const promises = userIds.map(async ({ userId }) => {
+        const result = await listDocuments('users', [
+            Query.equal('$id', userId),
+        ]);
+        return result.documents;
+    });
+
+    const users = await Promise.all(promises);
+
+    return { game, season, teams, players: users.flat() };
 }
