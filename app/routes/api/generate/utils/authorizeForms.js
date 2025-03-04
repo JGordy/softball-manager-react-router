1ca5e932f1581ee2f9bdb0790f4ed34757234158
Joseph Gordy <jgordy2424@gmail.com>
2025-03-04 12:18:01 -0500

    Refactor Google Forms authorization to use JWT for authentication

--- app/routes/api/generate/utils/authorizeForms.js
+++ app/routes/api/generate/utils/authorizeForms.js
@@ -1,5 +1,5 @@
 import { google } from 'googleapis';
-import { authenticate } from '@google-cloud/local-auth';
+import { JWT } from 'google-auth-library';
 
 export default async function authorize() {
     const credentialsString = process.env.GOOGLE_CREDENTIALS;
@@ -8,12 +8,16 @@ export default async function authorize() {
     }
     const credentials = JSON.parse(credentialsString);
 
-    const client = await authenticate({
-        credentials,
+    const client = new JWT({
+        email: credentials.client_email,
+        key: credentials.private_key,
         scopes: [
             'https://www.googleapis.com/auth/forms.body',
             'https://www.googleapis.com/auth/forms.responses.readonly',
         ],
     });
+
+    await client.authorize(); // Authorize the client
+
     return google.forms({ version: 'v1', auth: client });
 }
