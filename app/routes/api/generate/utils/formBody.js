428e8a90174aae1745e477b7d51cc3161db70a8f
Joseph Gordy <jgordy2424@gmail.com>
2025-03-04 12:18:12 -0500

    Update form body to include dynamic game date and opponent in title

--- app/routes/api/generate/utils/formBody.js
+++ app/routes/api/generate/utils/formBody.js
@@ -1,27 +1,31 @@
-export default {
-    requests: [
-        {
-            createItem: {
-                item: {
-                    title: 'Will you be attending?',
-                    questionItem: {
-                        question: {
-                            required: true,
-                            choiceQuestion: {
-                                type: 'RADIO',
-                                options: [
-                                    { value: 'Yes, I will be there' },
-                                    { value: 'No, I cannot attend' },
-                                    { value: 'Maybe, I will let you know' },
-                                ],
+import { formatDate } from '@/utils/dateTime';
+
+export default function getFormBody({ gameDate, opponent }) {
+    return {
+        requests: [
+            {
+                createItem: {
+                    item: {
+                        title: `Will you be attending the game on ${formatDate(new Date(gameDate))} against ${opponent || "TBD"}?`,
+                        questionItem: {
+                            question: {
+                                required: true,
+                                choiceQuestion: {
+                                    type: 'RADIO',
+                                    options: [
+                                        { value: 'Yes, I will be there' },
+                                        { value: 'No, I cannot attend' },
+                                        { value: 'Maybe, I will let you know' },
+                                    ],
+                                },
                             },
                         },
                     },
-                },
-                location: {
-                    index: 0,
+                    location: {
+                        index: 0,
+                    },
                 },
             },
-        },
-    ],
+        ],
+    };
 }
