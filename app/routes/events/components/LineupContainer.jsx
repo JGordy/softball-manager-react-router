2280c7b3fb590f8d9d7f38a5a6370d73b68adde6
Joseph Gordy <jgordy2424@gmail.com>
2025-03-05 15:15:18 -0500

    Add player count validation in LineupContainer to ensure a minimum of 8 players for lineup generation

--- app/routes/events/components/LineupContainer.jsx
+++ app/routes/events/components/LineupContainer.jsx
@@ -14,14 +14,24 @@ export default function LineupContainer({
     const [localChart, setLocalChart] = useState(parsedChart);
     console.log('/event/:eventId > LineupContainer: ', { availablePlayers, playerChart, parsedChart, localChart });
 
+    // NOTE: Most leagues require at least 8 players in the field to allow the teams to take the field
+    const hasEnoughPlayers = (availablePlayers?.length > 7);
+
+    let message = 'Charts for this game have not yet been created. You can create them below.';
+
+    if (!hasEnoughPlayers) {
+        message = 'You do not have enough players to generate a lineup. A minimum of 8 players is required.';
+    }
+
+    // hasEnoughPlayer
     return (
         <>
             {!localChart && (
                 <>
                     {managerView ? (
                         <>
-                            <Text align="center" c="dimmed" my="lg">Charts for this game have not yet been created. You can create them below.</Text>
-                            <Button mt="sm" onClick={handleGenerateCharts} fullWidth>
+                            <Text align="center" c={!hasEnoughPlayers ? "red" : "dimmed"} my="lg">{message}</Text>
+                            <Button mt="sm" onClick={handleGenerateCharts} disabled={!hasEnoughPlayers} fullWidth>
                                 Generate Batting and Fielding Charts
                             </Button>
                         </>
