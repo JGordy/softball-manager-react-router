b0c2a2f44a38b0ae86a77d93d065deca79027403
Joseph Gordy <jgordy2424@gmail.com>
2025-03-07 12:47:59 -0500

    Refactor Netlify server handler and update TypeScript paths for improved module resolution

--- .netlify/v1/functions/react-router-server.mjs
+++ .netlify/v1/functions/react-router-server.mjs
@@ -1,10 +1,9 @@
 
-    export { default } from "../../../build/server/server.js";
+export { default } from "../../../build/server/server.js";
 
-    export const config = {
-      name: "React Router server handler",
-      generator: "@netlify/vite-plugin-react-router@1.0.0",
-      path: "/*",
-      preferStatic: true,
-    };
-    
+export const config = {
+    name: "React Router server handler",
+    generator: "@netlify/vite-plugin-react-router@1.0.0",
+    path: "/*",
+    preferStatic: true,
+};
