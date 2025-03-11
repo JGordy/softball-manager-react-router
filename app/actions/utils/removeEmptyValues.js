a79eacc306124329a1ff778371d55b620d93da94
Joseph Gordy <jgordy2424@gmail.com>
2025-03-11 16:29:42 -0400

    Add utility function to remove undefined and empty string values from data

---
+++ app/actions/utils/removeEmptyValues.js
@@ -0,0 +1,11 @@
+export function removeEmptyValues({ values }) {
+    // Removes undefined or empty string values from data to update
+    const dataToUpdate = {};
+    for (const key in values) {
+        if (values.hasOwnProperty(key) && values[key] !== undefined && values[key] !== "") {
+            dataToUpdate[key] = values[key];
+        }
+    }
+
+    return dataToUpdate;
+}
