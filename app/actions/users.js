66eae51bebce91f7d25fd3a2ba58b58f8c320821
Joseph Gordy <jgordy2424@gmail.com>
2025-03-21 12:48:39 -0400

    Add createPlayer action to handle player creation and team membership

--- app/actions/users.js
+++ app/actions/users.js
@@ -3,6 +3,31 @@ import { createDocument, updateDocument } from '@/utils/databases.js';
 
 import { removeEmptyValues } from './utils/formUtils';
 
+export async function createPlayer({ values, teamId, userId }) {
+    try {
+        const _userId = userId || ID.unique(); // Create this now so it's easier to use later
+
+        const player = await createDocument(
+            'users',
+            _userId,
+            {
+                ...values,
+                preferredPositions: values.preferredPositions.split(","), // Split into an array of positions
+                dislikedPositions: values.dislikedPositions.split(","), // Split into an array of positions
+                userId,
+            },
+        );
+
+        // Create document in relationship table for the user and team id's.
+        await createDocument('memberships', null, { userId, teamId, role: 'player' });
+
+        return { response: { player }, status: 201, success: true };
+    } catch (error) {
+        console.error("Error creating player:", error);
+        throw error;
+    }
+}
+
 export async function registerUser({ values }) {
     // TODO: Register function
 };
