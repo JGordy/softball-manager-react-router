f6d609b0a95cbde44aa79c3cef0b368acc9a75cc
Joseph Gordy <jgordy2424@gmail.com>
2025-03-22 17:01:09 -0400

    Add getSeasonDetails function to load season data by seasonId

--- app/loaders/seasons.js
+++ app/loaders/seasons.js
@@ -1,11 +1,9 @@
 import { readDocument } from '@/utils/databases';
 
 export async function getSeasonDetails({ seasonId }) {
-
     if (seasonId) {
-        return {
-            season: await readDocument('seasons', seasonId),
-        };
+        // TODO: Add team name to games
+        return { season: await readDocument('seasons', seasonId) };
     } else {
         return { season: {} };
     }
