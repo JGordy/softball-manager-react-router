c9d74af58660c179b334da91912381ccd05d3485
Joseph Gordy <jgordy2424@gmail.com>
2025-03-23 14:43:56 -0400

    Add loaders for games and teams; rename getSeasonDetails to getSeasonById

--- app/loaders/seasons.js
+++ app/loaders/seasons.js
@@ -1,6 +1,6 @@
 import { readDocument } from '@/utils/databases';
 
-export async function getSeasonDetails({ seasonId }) {
+export async function getSeasonById({ seasonId }) {
     if (seasonId) {
         // TODO: Add team name to games
         return { season: await readDocument('seasons', seasonId) };
