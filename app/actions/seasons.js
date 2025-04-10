1833043d8c169f1793cf8cc7ee482109c9c333e6
Joseph Gordy <jgordy2424@gmail.com>
2025-04-10 16:02:28 -0400

    Integrate park creation logic into season creation and update actions

--- app/actions/seasons.js
+++ app/actions/seasons.js
@@ -1,21 +1,44 @@
 import { ID } from '@/appwrite';
 import { createDocument, updateDocument } from '@/utils/databases.js';
 
+import { findOrCreatePark } from '@/actions/parks';
+
 import { removeEmptyValues } from './utils/formUtils';
 
 export async function createSeason({ values, teamId }) {
     const { locationDetails, ...rest } = values;
-    console.log({ locationDetails });
+
+    let parsedLocationDetails = null;
+    try {
+        parsedLocationDetails = JSON.parse(locationDetails);
+    } catch (error) {
+        console.error("Error parsing locationDetails:", error);
+    }
 
     try {
         const seasonId = ID.unique(); // Create this now so it's easier to use later
+        let parkId;
+
+        if (parsedLocationDetails?.placeId) {
+            const parkResponse = await findOrCreatePark({
+                values: parsedLocationDetails,
+                placeId: parsedLocationDetails.placeId
+            });
+
+            console.log('actions/seasons.js ', { parkResponse });
+
+            if (parkResponse) {
+                parkId = parkResponse.$id;
+            }
+        }
 
         const season = await createDocument(
-            'seasons', // Your users collection ID
-            seasonId, // Generates a unique user ID in the handler
+            'seasons',
+            seasonId,
             {
                 ...rest,
-                gameDays: rest.gameDays.split(","), // Split into an array of positions
+                gameDays: rest.gameDays.split(","), // Split into an array of gameDays
+                parkId: parkId || null,
                 signUpFee: Number(rest.signUpFee),
                 teamId,
                 teams: [teamId],
@@ -32,18 +55,31 @@ export async function createSeason({ values, teamId }) {
 export async function updateSeason({ values, seasonId }) {
     const { locationDetails, ...rest } = values;
     console.log({ locationDetails });
-    // Removes undefined or empty string values from data to update
-    let dataToUpdate = removeEmptyValues({ values: rest });
 
-    if (dataToUpdate.gameDays) dataToUpdate.gameDays = dataToUpdate.gameDays.split(",");
-    if (dataToUpdate.signUpFee) dataToUpdate.signUpFee = Number(dataToUpdate.signUpFee);
-
-    try {
-        const seasonDetails = await updateDocument('seasons', seasonId, dataToUpdate);
-
-        return { response: { seasonDetails }, status: 204, success: true };
-    } catch (error) {
-        console.error("Error updating season:", error);
-        throw error;
+    let parkId;
+    if (locationDetails) {
+        const parsedLocationDetails = JSON.parse(locationDetails);
+        if (parsedLocationDetails?.placeId) {
+            const parkResponse = await findOrCreatePark({
+                values: parsedLocationDetails,
+                placeId: parsedLocationDetails.placeId
+            });
+            console.log({ parkResponse });
+        }
     }
+    // // Removes undefined or empty string values from data to update
+    // let dataToUpdate = removeEmptyValues({ values: rest });
+
+    // if (dataToUpdate.gameDays) dataToUpdate.gameDays = dataToUpdate.gameDays.split(",");
+    // if (dataToUpdate.signUpFee) dataToUpdate.signUpFee = Number(dataToUpdate.signUpFee);
+    // if (parkId) dataToUpdate.parkId = parkId;
+
+    // try {
+    //     const seasonDetails = await updateDocument('seasons', seasonId, dataToUpdate);
+
+    //     return { response: { seasonDetails }, status: 204, success: true };
+    // } catch (error) {
+    //     console.error("Error updating season:", error);
+    //     throw error;
+    // }
 }
