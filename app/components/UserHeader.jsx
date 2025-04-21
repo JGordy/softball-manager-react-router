513ededa14afa160fd7e8bf553c5eb8c03070ea7
Joseph Gordy <jgordy2424@gmail.com>
2025-04-21 13:43:19 -0400

    enhance UserHeader component to display email verification status and message

--- app/components/UserHeader.jsx
+++ app/components/UserHeader.jsx
@@ -2,17 +2,34 @@ import { useOutletContext } from 'react-router';
 
 import { Avatar, Group, Text, Title } from '@mantine/core';
 
+import { IconRosetteDiscountCheckFilled } from '@tabler/icons-react';
+
 export default function UserHeader({ subText }) {
-    const { user } = useOutletContext();
+    const context = useOutletContext();
+    const { user, isVerified } = context;
+    console.log('UserHeader', { context });
     const fullName = `${user.firstName} ${user.lastName}`;
 
     return (
-        <Group my="md">
-            <Avatar color="green" name={fullName} alt={fullName} size="md" />
-            <div>
-                <Title order={3}>{`Hello, ${user.firstName}!`}</Title>
-                {subText && <Text size="0.8rem">{subText}</Text>}
-            </div>
-        </Group>
+        <>
+            <Group my="md">
+                <Avatar color="green" name={fullName} alt={fullName} size="md" />
+                <div>
+                    <Title order={3}>
+                        <Group gap="0px">
+                            {`Hello, ${user.firstName}!`}
+                            {isVerified && <IconRosetteDiscountCheckFilled size={16} color="green" />}
+                        </Group>
+                    </Title>
+                    {subText && <Text size="0.8rem">{subText}</Text>}
+                </div>
+            </Group>
+
+            {!isVerified && (
+                <Text mt="md" c="red">
+                    Your email is not verified. Please check your inbox for a verification email.
+                </Text>
+            )}
+        </>
     );
 }
