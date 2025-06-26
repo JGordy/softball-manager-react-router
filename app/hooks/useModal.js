858b563bc2ccbdcc20196fc919210a95c9db2826
Joe Gordy <jgordy2424@gmail.com>
2025-06-26 17:11:44 -0400

    feat: add updateModal function to modify existing modals

--- app/hooks/useModal.js
+++ app/hooks/useModal.js
@@ -3,7 +3,7 @@ import { modals } from '@mantine/modals';
 const useModal = () => {
 
     const openModal = ({ title, children, ...rest }) => {
-        modals.open({
+        return modals.open({
             title,
             children,
             overlayProps: {
@@ -14,7 +14,20 @@ const useModal = () => {
         });
     };
 
-    return { openModal, closeAllModals: modals.closeAll };
+    const updateModal = (id, { title, children, ...rest }) => {
+        console.log({ id, title, children, ...rest });
+        modals.updateModal(id, {
+            title,
+            children,
+            overlayProps: {
+                backgroundOpacity: 0.55,
+                blur: 3,
+            },
+            ...rest,
+        });
+    }
+
+    return { openModal, closeAllModals: modals.closeAll, updateModal };
 };
 
 export default useModal;
