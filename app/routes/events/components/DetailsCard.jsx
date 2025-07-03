1e22d5ea70fdd2823fa772efcedc709f0bf9f388
Joe Gordy <jgordy2424@gmail.com>
2025-07-03 14:27:01 -0400

    feat: add DetailsCard component to display game details and location

---
+++ app/routes/events/components/DetailsCard.jsx
@@ -0,0 +1,49 @@
+import {
+    Card,
+    Divider,
+    Group,
+} from '@mantine/core';
+
+import {
+    IconChevronRight,
+    IconClock,
+    IconMapPin,
+} from '@tabler/icons-react';
+
+export default function DetailsCard({
+    formattedGameTime,
+    park,
+    season,
+    locationDrawerHandlers = {},
+}) {
+
+    return (
+        <Card withBorder radius="md" mt="xl" py="0px">
+            <Card.Section my="xs" inheritPadding>
+                <Group gap="xs">
+                    <IconClock size={18} />
+                    {formattedGameTime}
+                </Group>
+            </Card.Section>
+
+            <Divider />
+
+            <Card.Section my="xs" inheritPadding>
+                {park?.googleMapsURI ? (
+                    <Group justify='space-between' onClick={locationDrawerHandlers.open} c="green">
+                        <Group gap="xs">
+                            <IconMapPin size={18} />
+                            {season?.location}
+                        </Group>
+                        <IconChevronRight size={18} />
+                    </Group>
+                ) : (
+                    <Group gap="xs">
+                        <IconMapPin size={18} />
+                        {season?.location}
+                    </Group>
+                )}
+            </Card.Section>
+        </Card>
+    );
+}
