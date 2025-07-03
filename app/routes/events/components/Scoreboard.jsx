c26623e1649221d189f38abc51a2dc9197c3898d
Joe Gordy <jgordy2424@gmail.com>
2025-07-03 14:26:56 -0400

    feat: add Scoreboard component to display game scores and details

---
+++ app/routes/events/components/Scoreboard.jsx
@@ -0,0 +1,48 @@
+import {
+    Card,
+    Center,
+    Group,
+    Paper,
+    Text,
+    Title,
+} from '@mantine/core';
+
+export default function Scoreboard({
+    game = {},
+    gameIsPast,
+    team,
+}) {
+
+    const {
+        score,
+        opponent,
+        opponentScore,
+        isHomeGame,
+        result,
+    } = game;
+
+    return (
+        <Paper mt="xl">
+            <Title order={4} align="center" mb="sm">
+                {team?.name} {isHomeGame ? 'vs' : '@'} {opponent || "TBD"}
+            </Title>
+            <Group justify="center" gap="xl" align="center">
+                <Card withBorder radius="md" px="xl">
+                    <Text>{score || '0'}</Text>
+                </Card>
+
+                <div>-</div>
+
+                <Card withBorder radius="md" px="xl">
+                    <Text>{opponentScore || '0'}</Text>
+                </Card>
+            </Group>
+
+            {gameIsPast && !result && (
+                <Center mt="md">
+                    <Text size="sm" c="yellow">Game result pending*</Text>
+                </Center>
+            )}
+        </Paper>
+    );
+}
