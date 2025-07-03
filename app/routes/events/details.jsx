c4f4a1db9d9754a2790e09b6097d753e64d74fba
Joe Gordy <jgordy2424@gmail.com>
2025-07-03 14:27:11 -0400

    feat: refactor EventDetails component to integrate MenuContainer, Scoreboard, and DetailsCard components

--- app/routes/events/details.jsx
+++ app/routes/events/details.jsx
@@ -3,46 +3,36 @@ import { useEffect } from 'react';
 import { useOutletContext } from 'react-router';
 
 import {
-    ActionIcon,
     Anchor,
+    Button,
     Card,
-    Center,
-    Divider,
     Flex,
     Group,
-    Menu,
-    Paper,
     Tabs,
     Text,
-    Title,
 } from '@mantine/core';
 import { useDisclosure, useClipboard } from '@mantine/hooks';
 
 import {
-    IconChevronRight,
-    IconClock,
     IconCopy,
-    IconDots,
-    IconEdit,
     IconLocationFilled,
     IconMapPin,
-    IconScoreboard,
-    IconTrashX,
 } from '@tabler/icons-react';
 
 import BackButton from '@/components/BackButton';
 import DrawerContainer from '@/components/DrawerContainer';
 
-import AddGameResults from '@forms/AddGameResults';
-import AddSingleGame from '@/forms/AddSingleGame';
 import { createAttendanceForm, updateGame } from '@/actions/games';
 
 import { getEventById } from '@/loaders/games';
 
-import { formatGameTime, formatTime, getGameDayStatus } from '@/utils/dateTime';
+import { formatGameTime, getGameDayStatus } from '@/utils/dateTime';
 
 import useModal from '@/hooks/useModal';
 
+import MenuContainer from './components/MenuContainer';
+import Scoreboard from './components/Scoreboard';
+import DetailsCard from './components/DetailsCard';
 import AvailabliityContainer from './components/AvailabliityContainer';
 import LineupContainer from './components/LineupContainer';
 
@@ -99,8 +89,10 @@ export async function loader({ params, request }) {
 export default function EventDetails({ loaderData, actionData }) {
     console.log('/events/:eventId > ', { ...loaderData });
 
-    const [opened, { open, close }] = useDisclosure(false);
-    const { openModal, closeAllModals } = useModal();
+    const [locationDrawerOpened, locationDrawerHandlers] = useDisclosure(false);
+    const [deleteDrawerOpened, deleteDrawerHandlers] = useDisclosure(false);
+
+    const { closeAllModals } = useModal();
 
     const clipboard = useClipboard({ timeout: 500 });
 
@@ -122,12 +114,8 @@ export default function EventDetails({ loaderData, actionData }) {
 
     const {
         gameDate,
-        isHomeGame,
-        opponent,
-        opponentScore,
         playerChart,
         result,
-        score,
         timeZone,
     } = game;
 
@@ -160,126 +148,33 @@ export default function EventDetails({ loaderData, actionData }) {
         handleAfterSubmit();
     }, [actionData]);
 
-    const openEditGameModal = () => openModal({
-        title: 'Update Game Details',
-        children: (
-            <AddSingleGame
-                action="update-game"
-                actionRoute={`/events/${game.$id}`}
-                defaults={{
-                    isHomeGame: 'false',
-                    gameTime: formatTime(game.gameDate, game.timeZone),
-                    gameDate: game.gameDate,
-                }}
-                teamId={team.$id}
-                seasonId={season.$id}
-                confirmText="Update Game"
-            />
-        ),
-    });
-
-    const openGameResultsModal = () => openModal({
-        title: 'Add Results for this game',
-        children: (
-            <AddGameResults
-                actionRoute={`/events/${game.$id}`}
-                teamId={team.$id}
-                defaults={{
-                    score: game?.score || 0,
-                    opponentScore: game?.opponentScore || 0,
-                    result: game?.result || null,
-                }}
-            />
-        ),
-    });
-
-    const eventSettingsDropdown = (
-        <Menu shadow="md" withArrow offset={0}>
-            <Menu.Target>
-                <ActionIcon variant="light" radius="xl" size="lg">
-                    <IconDots />
-                </ActionIcon>
-            </Menu.Target>
-            <Menu.Dropdown>
-                <Menu.Label>
-                    <Text size="xs">Game Details</Text>
-                </Menu.Label>
-                {gameIsPast && (
-                    <Menu.Item onClick={openGameResultsModal} leftSection={<IconScoreboard size={14} />}>
-                        <Text>{`${result ? 'Update' : 'Add'} game results`}</Text>
-                    </Menu.Item>
-                )}
-                <Menu.Item onClick={openEditGameModal} leftSection={<IconEdit size={14} />}>
-                    <Text>Edit Game Details</Text>
-                </Menu.Item>
-                <Menu.Divider />
-                <Menu.Label>
-                    <Text size="xs">Danger zone</Text>
-                </Menu.Label>
-                <Menu.Item leftSection={<IconTrashX size={14} />} color="red">
-                    <Text>Delete Game</Text>
-                </Menu.Item>
-            </Menu.Dropdown>
-        </Menu>
-    );
-
     return (
         <>
             <Group justify="space-between">
                 <BackButton text="Back to Events" />
-                {managerView && eventSettingsDropdown}
+                {managerView && (
+                    <MenuContainer
+                        game={game}
+                        gameIsPast={gameIsPast}
+                        openDeleteDrawer={deleteDrawerHandlers.open}
+                        result={result}
+                        team={team}
+                    />
+                )}
             </Group>
 
-            <Paper mt="xl">
-                <Title order={4} align="center" mb="sm">
-                    {team?.name} {isHomeGame ? 'vs' : '@'} {opponent || "TBD"}
-                </Title>
-                <Group justify="center" gap="xl" align="center">
-                    <Card withBorder radius="md" px="xl">
-                        <Text>{score || '0'}</Text>
-                    </Card>
+            <Scoreboard
+                game={game}
+                gameIsPast={gameIsPast}
+                team={team}
+            />
 
-                    <div>-</div>
-
-                    <Card withBorder radius="md" px="xl">
-                        <Text>{opponentScore || '0'}</Text>
-                    </Card>
-                </Group>
-
-                {gameIsPast && !result && (
-                    <Center mt="md">
-                        <Text size="sm" c="yellow">Game result pending*</Text>
-                    </Center>
-                )}
-            </Paper>
-
-            <Card withBorder radius="md" mt="xl" py="0px">
-                <Card.Section my="xs" inheritPadding>
-                    <Group gap="xs">
-                        <IconClock size={18} />
-                        {formattedGameTime}
-                    </Group>
-                </Card.Section>
-
-                <Divider />
-
-                <Card.Section my="xs" inheritPadding>
-                    {park?.googleMapsURI ? (
-                        <Group justify='space-between' onClick={open} c="green">
-                            <Group gap="xs">
-                                <IconMapPin size={18} />
-                                {season?.location}
-                            </Group>
-                            <IconChevronRight size={18} />
-                        </Group>
-                    ) : (
-                        <Group gap="xs">
-                            <IconMapPin size={18} />
-                            {season?.location}
-                        </Group>
-                    )}
-                </Card.Section>
-            </Card>
+            <DetailsCard
+                formattedGameTime={formattedGameTime}
+                park={park}
+                season={season}
+                locationDrawerHandlers={locationDrawerHandlers}
+            />
 
             <Tabs radius="md" defaultValue={(availablePlayers?.length > 7) ? 'lineup' : 'availability'} mt="xl">
                 <Tabs.List grow justify="center">
@@ -311,10 +206,21 @@ export default function EventDetails({ loaderData, actionData }) {
                 </Tabs.Panel>
             </Tabs>
 
+            {managerView && (
+                <DrawerContainer
+                    opened={deleteDrawerOpened}
+                    onClose={deleteDrawerHandlers.close}
+                    title="Delete Game"
+                >
+                    <Text>Are you sure you want to delete this game? There is no undoing this action.</Text>
+                    <Button color="red" variant="filled" mt="lg" fullWidth>Yes, Delete this Game</Button>
+                </DrawerContainer>
+            )}
+
             {park && (
                 <DrawerContainer
-                    opened={opened}
-                    onClose={close}
+                    opened={locationDrawerOpened}
+                    onClose={locationDrawerHandlers.close}
                     title="Location Details"
                 >
                     <Flex align="center" gap="md" mb="xl">
