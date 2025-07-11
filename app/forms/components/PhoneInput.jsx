c085282ade606a352def47daae5a91be7cf4acad
Joe Gordy <jgordy2424@gmail.com>
2025-07-11 13:20:59 -0400

    feat: add PhoneInput component for formatted phone number input

---
+++ app/forms/components/PhoneInput.jsx
@@ -0,0 +1,22 @@
+import { Input } from "@mantine/core";
+
+import { IMaskInput } from 'react-imask';
+
+import classes from '@/styles/inputs.module.css';
+
+export default function PhoneInput({ defaultValue }) {
+
+    return (
+        <Input.Wrapper className={classes.inputs}>
+            <Input.Label>Phone Number</Input.Label>
+            <Input
+                defaultValue={defaultValue}
+                component={IMaskInput}
+                label="Phone Number"
+                mask="(000) 000-0000"
+                name="phoneNumber"
+                placeholder='(xxx) xxx-xxxx'
+            />
+        </Input.Wrapper>
+    );
+};
