3be261faf8469e24c13e448c66f58f1f2101b040
Joe Gordy <jgordy2424@gmail.com>
2025-07-12 15:13:21 -0400

    feat: enhance password update form with reset option and email handling

--- app/forms/UpdatePassword.jsx
+++ app/forms/UpdatePassword.jsx
@@ -1,4 +1,6 @@
-import { PasswordInput } from '@mantine/core';
+import { PasswordInput, Text } from '@mantine/core';
+
+import AutocompleteEmail from '@/components/AutocompleteEmail';
 
 import FormWrapper from './FormWrapper';
 
@@ -8,6 +10,7 @@ export default function UpdatePassword({
     action = 'update-password',
     actionRoute,
     confirmText = 'Update Password',
+    user = {},
 }) {
 
     return (
@@ -17,26 +20,37 @@ export default function UpdatePassword({
             buttonColor="green"
             confirmText={confirmText}
         >
-            <PasswordInput
-                className={classes.inputs}
-                type="password"
-                name="currentPassword"
-                label="Current Password"
-                placeholder="Your password"
-                description="Used to verify your identity"
-                mt="md"
-                withAsterisk
-            />
-            <PasswordInput
-                className={classes.inputs}
-                type="password"
-                name="newPassword"
-                label="New Password"
-                placeholder="Your new password"
-                description="Must be at least 8 characters long"
-                mt="md"
-                withAsterisk
-            />
+            {action === 'password-reset' ? (
+                <>
+                    <Text size="sm" mb="md" c="yellow">
+                        Continue? We&apos;ll send a link to reset your password to your associated email address.
+                    </Text>
+                    <input type="hidden" name="email" value={user.email} />
+                </>
+            ) : (
+                <>
+                    <PasswordInput
+                        className={classes.inputs}
+                        type="password"
+                        name="currentPassword"
+                        label="Current Password"
+                        placeholder="Your password"
+                        description="Used to verify your identity"
+                        mt="md"
+                        withAsterisk
+                    />
+                    <PasswordInput
+                        className={classes.inputs}
+                        type="password"
+                        name="newPassword"
+                        label="New Password"
+                        placeholder="Your new password"
+                        description="Must be at least 8 characters long"
+                        mt="md"
+                        withAsterisk
+                    />
+                </>
+            )}
         </FormWrapper>
     );
 };
