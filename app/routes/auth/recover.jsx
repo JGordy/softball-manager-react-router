f8f8f041d2fe8a70302048995e4c615b6c77c4f9
Joe Gordy <jgordy2424@gmail.com>
2025-07-13 14:11:03 -0400

    Create base password recovery route/page

---
+++ app/routes/auth/recover.jsx
@@ -0,0 +1,82 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+
+import { PasswordInput } from '@mantine/core';
+import FormWrapper from '@/components/FormWrapper';
+
+// import { account } from '@/appwrite';
+
+import classes from '@/styles/inputs.module.css';
+
+export async function clientAction({ request }) {
+    const formData = await request.formData();
+    const userId = formData.get("userId");
+    const secret = formData.get("secret");
+    const password = formData.get("password");
+
+    console.log({ userId, secret, password });
+
+    // const result = await account.updateRecovery(
+    //     userId, // userId
+    //     secret, // secret
+    //     password, // new password
+    // );
+
+    // const response = await login({ email, password });
+
+    // if (response?.error) {
+    //     return { error: response.error?.message || response.error };
+    // }
+
+    // return { email, password, session: response.session };
+}
+
+export default function Verify({ actionData }) {
+    const [searchParams] = useSearchParams();
+
+    const secret = searchParams.get('secret');
+    const userId = searchParams.get('userId');
+
+    useEffect(() => {
+        if (actionData?.success) {
+            // Do something
+        }
+
+        if (actionData?.status === 500) {
+            // Do something
+        }
+    }, [actionData]);
+
+    return (
+        <div className="verification-container">
+            <h1>Password Reset</h1>
+            <FormWrapper
+                action="reset-password"
+                actionRoute="/recovery"
+                confirmText="Submit new password"
+                hideButtons
+            >
+                <input type="hidden" name="secret" value={secret} />
+                <input type="hidden" name="userId" value={userId} />
+                <PasswordInput
+                    className={classes.inputs}
+                    type="password"
+                    name="newPassword"
+                    label="New Password"
+                    placeholder="Your new password"
+                    description="Must be at least 8 characters long"
+                    mt="md"
+                    withAsterisk
+                />
+                <Button
+                    type="submit"
+                    color={buttonColor}
+                    autoContrast
+                    fullWidth
+                >
+                    {confirmText}
+                </Button>
+            </FormWrapper>
+        </div>
+    );
+};
