d7d60a5f2092ff8f365b0325afb060d3511f429b
Joe Gordy <jgordy2424@gmail.com>
2025-07-16 14:21:27 -0400

    Remove auth options from Account panel

--- app/routes/settings/components/AccountPanel.jsx
+++ app/routes/settings/components/AccountPanel.jsx
@@ -5,46 +5,31 @@ import { useOutletContext } from 'react-router';
 import {
     ActionIcon,
     Alert,
-    Button,
-    Divider,
     Group,
     Text,
 } from '@mantine/core';
 
-import { useDisclosure } from '@mantine/hooks';
-
 import {
-    IconLogout2,
     IconPencil,
     IconMail,
     IconPhone,
-    IconKey,
-    IconRestore,
 } from '@tabler/icons-react';
 
 import useModal from '@/hooks/useModal';
 
 import UpdateContactInfo from '@/forms/UpdateContactInfo';
-import UpdatePassword from '@/forms/UpdatePassword';
-import DrawerContainer from '@/components/DrawerContainer';
 
-export default function AccountPanel({ actionData, openLogoutDrawer }) {
-    // console.log({ actionData });
+export default function AccountPanel({ actionData }) {
 
     const { user } = useOutletContext();
 
     const { openModal, closeAllModals } = useModal();
-
-    const [passwordResetOpened, { open: openResetPassword, close: closeResetPassword }] = useDisclosure();
 
     const [formError, setFormError] = useState(null);
     const [actionSuccess, setActionSuccess] = useState(false);
 
     useEffect(() => {
         if (actionData?.success) {
-            if (passwordResetOpened) {
-                closeResetPassword();
-            }
             closeAllModals();
             setFormError(null);
             setActionSuccess(actionData.message);
@@ -73,11 +58,6 @@ export default function AccountPanel({ actionData, openLogoutDrawer }) {
         ),
     });
 
-    const openUpdatePasswordModal = () => openModal({
-        title: 'Update Your Password',
-        children: <UpdatePassword actionRoute='/settings' />,
-    });
-
     return (
         <>
             {actionSuccess && (
@@ -101,38 +81,6 @@ export default function AccountPanel({ actionData, openLogoutDrawer }) {
                     {formError}
                 </Alert>
             )}
-
-            <Text size="sm" mb="sm">Authentication</Text>
-
-            <Group justify="space-between">
-                <Text>Change Password</Text>
-                <ActionIcon
-                    variant="subtle"
-                    color="gray"
-                    aria-label="Update Password"
-                    style={{ cursor: 'pointer' }}
-                    onClick={openUpdatePasswordModal}
-                    size="lg"
-                >
-                    <IconKey size={20} />
-                </ActionIcon>
-            </Group>
-
-            <Group justify="space-between">
-                <Text>Reset Password</Text>
-                <ActionIcon
-                    variant="subtle"
-                    color="gray"
-                    aria-label="Reset Password"
-                    style={{ cursor: 'pointer' }}
-                    onClick={openResetPassword}
-                    size="lg"
-                >
-                    <IconRestore size={20} />
-                </ActionIcon>
-            </Group>
-
-            <Divider my="sm" />
 
             <Text size="sm" mb="sm">Contact Details</Text>
             <Group justify="space-between">
@@ -162,34 +110,6 @@ export default function AccountPanel({ actionData, openLogoutDrawer }) {
                     <IconPencil />
                 </ActionIcon>
             </Group>
-
-            <Divider my="sm" />
-
-            <Button
-                color="red"
-                onClick={openLogoutDrawer}
-                variant="subtle"
-                px="0px"
-                size="md"
-            >
-                <Group gap="xs">
-                    <IconLogout2 size={16} mr='xs' />
-                    Log out
-                </Group>
-            </Button>
-
-            <DrawerContainer
-                opened={passwordResetOpened}
-                onClose={closeResetPassword}
-                title="Reset Password"
-            >
-                <UpdatePassword
-                    action="password-reset"
-                    actionRoute='/settings'
-                    confirmText="Yes, Reset my Password"
-                    user={user}
-                />
-            </DrawerContainer>
         </>
     );
 }
