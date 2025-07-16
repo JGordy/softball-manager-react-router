8f3625557d51dd8d4ed45ac19aa4fd54a8bcad69
Joe Gordy <jgordy2424@gmail.com>
2025-07-16 14:21:12 -0400

    Abstract Auth panel logic to seperate container

---
+++ app/routes/settings/components/AuthPanel.jsx
@@ -0,0 +1,144 @@
+import { useEffect, useState } from 'react';
+
+import { useOutletContext } from 'react-router';
+
+import {
+    ActionIcon,
+    Alert,
+    Button,
+    Divider,
+    Group,
+    Text,
+} from '@mantine/core';
+
+import { useDisclosure } from '@mantine/hooks';
+
+import {
+    IconLogout2,
+    IconKey,
+    IconRestore,
+} from '@tabler/icons-react';
+
+import useModal from '@/hooks/useModal';
+
+import UpdatePassword from '@/forms/UpdatePassword';
+import DrawerContainer from '@/components/DrawerContainer';
+
+export default function AuthPanel({ actionData, openLogoutDrawer }) {
+    // console.log({ actionData });
+
+    const { user } = useOutletContext();
+
+    const { openModal, closeAllModals } = useModal();
+
+    const [passwordResetOpened, { open: openResetPassword, close: closeResetPassword }] = useDisclosure();
+
+    const [formError, setFormError] = useState(null);
+    const [actionSuccess, setActionSuccess] = useState(false);
+
+    useEffect(() => {
+        if (actionData?.success) {
+            if (passwordResetOpened) {
+                closeResetPassword();
+            }
+            closeAllModals();
+            setFormError(null);
+            setActionSuccess(actionData.message);
+        }
+
+        if (actionData?.status === 500) {
+            setTimeout(() => {
+                setFormError(actionData.message);
+                setActionSuccess(null);
+                closeAllModals();
+            }, 300);
+        }
+    }, [actionData, closeAllModals]);
+
+    const openUpdatePasswordModal = () => openModal({
+        title: 'Update Your Password',
+        children: <UpdatePassword actionRoute='/settings' />,
+    });
+
+    return (
+        <>
+            {actionSuccess && (
+                <Alert
+                    mb="md"
+                    variant="light"
+                    color="green"
+                    title="Success!"
+                >
+                    {actionSuccess}
+                </Alert>
+            )}
+
+            {formError && (
+                <Alert
+                    mb="md"
+                    variant="light"
+                    color="red"
+                    title="Invalid Form Submission"
+                >
+                    {formError}
+                </Alert>
+            )}
+
+            <Group justify="space-between">
+                <Text>Change Password</Text>
+                <ActionIcon
+                    variant="subtle"
+                    color="gray"
+                    aria-label="Update Password"
+                    style={{ cursor: 'pointer' }}
+                    onClick={openUpdatePasswordModal}
+                    size="lg"
+                >
+                    <IconKey size={20} />
+                </ActionIcon>
+            </Group>
+
+            <Group justify="space-between">
+                <Text>Reset Password</Text>
+                <ActionIcon
+                    variant="subtle"
+                    color="gray"
+                    aria-label="Reset Password"
+                    style={{ cursor: 'pointer' }}
+                    onClick={openResetPassword}
+                    size="lg"
+                >
+                    <IconRestore size={20} />
+                </ActionIcon>
+            </Group>
+
+            <Divider my="sm" />
+
+            <Button
+                color="red"
+                onClick={openLogoutDrawer}
+                variant="subtle"
+                px="0px"
+                size="md"
+            >
+                <Group gap="xs">
+                    <IconLogout2 size={16} mr='xs' />
+                    Log out
+                </Group>
+            </Button>
+
+            <DrawerContainer
+                opened={passwordResetOpened}
+                onClose={closeResetPassword}
+                title="Reset Password"
+            >
+                <UpdatePassword
+                    action="password-reset"
+                    actionRoute='/settings'
+                    confirmText="Yes, Reset my Password"
+                    user={user}
+                />
+            </DrawerContainer>
+        </>
+    );
+}
