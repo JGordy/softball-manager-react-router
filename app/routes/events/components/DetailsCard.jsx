242c41954beadbc001b5a84b04a06978149048e7
Joe Gordy <jgordy2424@gmail.com>
2025-07-18 16:55:19 -0400

    Refactor DetailsCard to abstract Calendar and Park drawer logic

--- app/routes/events/components/DetailsCard.jsx
+++ app/routes/events/components/DetailsCard.jsx
@@ -3,6 +3,7 @@ import {
     Divider,
     Group,
 } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 import {
     IconChevronRight,
@@ -10,40 +11,75 @@ import {
     IconMapPin,
 } from '@tabler/icons-react';
 
-export default function DetailsCard({
-    formattedGameTime,
-    park,
-    season,
-    locationDrawerHandlers = {},
-}) {
+import { formatGameTime } from '@/utils/dateTime';
+
+import DrawerContainer from '@/components/DrawerContainer';
+
+import ParkDetailsDrawer from './ParkDetailsDrawer';
+import CalendarDetails from './CalendarDetails';
+
+export default function DetailsCard({ game, park, season }) {
+
+    const {
+        gameDate,
+        timeZone,
+    } = game;
+
+    const formattedGameTime = formatGameTime(gameDate, timeZone);
+
+    const [locationDrawerOpened, locationDrawerHandlers] = useDisclosure(false);
+    const [calendarDrawerOpened, calendarDrawerHandlers] = useDisclosure(false);
 
     return (
-        <Card withBorder radius="lg" mt="-12%" mx="md" py="5px">
-            <Card.Section my="xs" inheritPadding>
-                <Group gap="xs">
-                    <IconClock size={18} />
-                    {formattedGameTime}
-                </Group>
-            </Card.Section>
+        <>
+            <Card withBorder radius="lg" mt="-12%" mx="md" py="5px">
+                <Card.Section my="xs" inheritPadding>
+                    <Group justify="space-between" c="green" onClick={calendarDrawerHandlers.open}>
+                        <Group gap="xs" c="green">
+                            <IconClock size={18} />
+                            {formattedGameTime}
+                        </Group>
+                        <IconChevronRight size={18} />
+                    </Group>
+                </Card.Section>
 
-            <Divider />
+                <Divider />
 
-            <Card.Section my="xs" inheritPadding>
-                {park?.googleMapsURI ? (
-                    <Group justify='space-between' onClick={locationDrawerHandlers.open} c="green">
+                <Card.Section my="xs" inheritPadding>
+                    {park?.googleMapsURI ? (
+                        <Group justify='space-between' onClick={locationDrawerHandlers.open} c="green">
+                            <Group gap="xs">
+                                <IconMapPin size={18} />
+                                {season?.location}
+                            </Group>
+                            <IconChevronRight size={18} />
+                        </Group>
+                    ) : (
                         <Group gap="xs">
                             <IconMapPin size={18} />
                             {season?.location}
                         </Group>
-                        <IconChevronRight size={18} />
-                    </Group>
-                ) : (
-                    <Group gap="xs">
-                        <IconMapPin size={18} />
-                        {season?.location}
-                    </Group>
-                )}
-            </Card.Section>
-        </Card>
+                    )}
+                </Card.Section>
+            </Card>
+
+            <DrawerContainer
+                opened={calendarDrawerOpened}
+                onClose={calendarDrawerHandlers.close}
+                title="Date & Time Details"
+            >
+                <CalendarDetails game={game} />
+            </DrawerContainer>
+
+            {park && (
+                <DrawerContainer
+                    opened={locationDrawerOpened}
+                    onClose={locationDrawerHandlers.close}
+                    title="Location Details"
+                >
+                    <ParkDetailsDrawer park={park} />
+                </DrawerContainer>
+            )}
+        </>
     );
 }
