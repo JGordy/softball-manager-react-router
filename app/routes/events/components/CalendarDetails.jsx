2e3e39b026e50088642f84c423358b317486ea9d
Joe Gordy <jgordy2424@gmail.com>
2025-07-21 15:49:05 -0400

    Refactor CalendarDetails component to improve calendar link handling and layout

--- app/routes/events/components/CalendarDetails.jsx
+++ app/routes/events/components/CalendarDetails.jsx
@@ -1,27 +1,68 @@
-import { Anchor, Card, Group, Text } from '@mantine/core';
-import { IconLocationFilled } from '@tabler/icons-react';
+import {
+    Anchor,
+    Card,
+    Group,
+    Text,
+} from '@mantine/core';
+import { useOs } from '@mantine/hooks';
+
+import {
+    IconBrandApple,
+    IconBrandGoogleFilled,
+    IconBrandOffice,
+    IconBrandWindowsFilled,
+} from '@tabler/icons-react';
 
 import { google, ics, outlook, office365 } from "calendar-link";
 
 const calendarMap = {
-    'Google': google,
-    'Apple': ics,
-    'Outlook': outlook,
-    'Office 365': office365,
+    google: {
+        href: google,
+        icon: <IconBrandGoogleFilled size={18} />,
+        label: 'Google',
+    },
+    apple: {
+        href: ics,
+        icon: <IconBrandApple size={18} />,
+        label: 'Apple',
+    },
+    outlook: {
+        href: outlook,
+        icon: <IconBrandWindowsFilled size={18} />,
+        label: 'Outlook',
+    },
+    office365: {
+        href: office365,
+        icon: <IconBrandOffice size={18} />,
+        label: 'Office',
+    },
 };
 
 export default function CalendarDetails({ game, park, team }) {
-    console.log({ game });
+
+    const os = useOs();
+    const isGoogle = ['android', 'chromeos'].includes(os);
+    const isApple = ['ios', 'macos'].includes(os);
+    const isMicrosoft = ['windows'].includes(os);
+
+    const calendarOrder = () => {
+        if (isGoogle) return ['google', 'apple', 'outlook', 'office365'];
+        if (isApple) return ['apple', 'google', 'outlook', 'office365'];
+        if (isMicrosoft) return ['outlook', 'office365', 'google', 'apple'];
+        return ['google', 'apple', 'outlook', 'office365'];
+    };
+
     const {
         isHomeGame,
         opponent,
         gameDate,
+        location,
         timeZone,
     } = game;
 
     const event = {
         title: `${team?.name} ${isHomeGame ? 'vs' : '@'} ${opponent || "TBD"}`,
-        location: park?.formattedAddress || '',
+        location: park?.formattedAddress || location || '',
         start: gameDate,
         duration: [1, 'hour'],
         timeZone: timeZone,
@@ -30,22 +71,29 @@ export default function CalendarDetails({ game, park, team }) {
     return (
         <>
             <div>Add game to calendar</div>
-            {['Google', 'Apple', 'Outlook', 'Office 365'].map((key) => (
-                <Anchor
-                    href={calendarMap[key](event)}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    mb="md"
-                    key={key}
-                >
-                    <Card c="green" mb="md">
-                        <Group gap="xs">
-                            <IconLocationFilled size={18} />
-                            <Text>{key}</Text>
-                        </Group>
-                    </Card>
-                </Anchor>
-            ))}
+            <Group gap="xs" mt="md" justify='space-between'>
+                {calendarOrder().map((key, index) => {
+                    const { href, icon, label } = calendarMap[key];
+                    const isFirstItem = index === 0;
+
+                    return (
+                        <Anchor
+                            key={key}
+                            href={href(event)}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            w={isFirstItem ? '100%' : 'auto'}
+                        >
+                            <Card c="green">
+                                <Group gap="xs" justify='center' mr={isFirstItem ? '10px' : ''}>
+                                    {icon}
+                                    <Text>{label}</Text>
+                                </Group>
+                            </Card>
+                        </Anchor>
+                    );
+                })}
+            </Group>
         </>
     );
 };
