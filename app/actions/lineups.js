32a933dcc0f8f3beb98ef77f7c1d4442b91059c0
Joe Gordy <jgordy2424@gmail.com>
2025-08-01 19:07:53 -0400

    parse then stringify playerchart

--- app/actions/lineups.js
+++ app/actions/lineups.js
@@ -35,7 +35,7 @@ export async function savePlayerChart({ values, eventId }) {
     // NOTE: We must stringify the playerChart json before sending to appwrite
     try {
         const gameDetails = await updateDocument('games', eventId, {
-            playerChart: JSON.stringify(playerChart),
+            playerChart: JSON.stringify(JSON.parse(playerChart)),
         });
 
         return { response: { gameDetails }, status: 204, success: true };
