e9091ca5cd3d0d367df2593b7eeee36d9d16c3ff
Joe Gordy <jgordy2424@gmail.com>
2025-08-01 17:13:30 -0400

    parse playerChart

--- app/loaders/games.js
+++ app/loaders/games.js
@@ -43,7 +43,7 @@ export async function getEventById({ request, eventId }) {
 
     return {
         deferredData,
-        game,
+        game: { ...game, playerChart: JSON.parse(game.playerChart) },
         managerId,
         season,
         teams,
