507707c7a761e5a8625d2dcbad27c70eef57b57a
Joe Gordy <jgordy2424@gmail.com>
2025-08-04 16:46:19 -0400

    Separate a view only batting order and field chart from an editable version

--- app/components/PlayerChart.jsx
+++ app/components/PlayerChart.jsx
@@ -1,83 +1,16 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import { useMemo } from 'react';
+
 import {
-    Group,
     ScrollArea,
-    Select,
     Table,
     Text,
 } from '@mantine/core';
-
-import {
-    IconCheck,
-    IconGripVertical,
-} from '@tabler/icons-react';
-
-import { Draggable, Droppable } from '@hello-pangea/dnd';
 
 import fieldingPositions from '@/constants/positions';
 
 import styles from '../styles/playerChart.module.css';
 
-const PositionSelect = React.memo(({
-    row,
-    inning,
-    handlePositionChange,
-    positionData,
-    playerChart,
-}) => {
-
-    const playerLookup = useMemo(() => {  // Create lookup map
-        const lookup = {};
-        playerChart.forEach(player => {
-            lookup[player.$id] = player;
-        });
-        return lookup;
-    }, [playerChart]);
-
-    const renderSelectOption = useCallback(({ option, checked }) => {
-
-        const player = playerLookup[row.playerId];
-
-        let color = 'gray';
-
-        const preferredPositions = player?.preferredPositions;
-        if (preferredPositions?.includes(option.value)) {
-            color = 'green';
-        }
-
-        const dislikedPositions = player?.dislikedPositions;
-        if (dislikedPositions?.includes(option.value)) {
-            color = 'red';
-        }
-
-        return (
-            <Group gap="xs">
-                {checked && <IconCheck style={{ color }} size={16} />}
-                <Text style={{ color }}>{option.label}</Text>
-            </Group>
-        );
-    }, [playerChart]);
-
-    return (
-        <Select
-            key={`${row.player}-${inning}`}
-            value={row[inning]}
-            onChange={(event) => handlePositionChange(event, row.playerId, inning)}
-            data={positionData}
-            style={{ minWidth: '160px' }}
-            renderOption={renderSelectOption}
-        />
-    );
-});
-
-const PlayerChart = ({
-    playerChart,
-    setPlayerChart,
-    managerView = false,
-}) => {
-
-    const [inningPositions, setInningPositions] = useState({});
-    console.log({ playerChart, inningPositions });
+const PlayerChart = ({ playerChart }) => {
 
     const columns = useMemo(() => [
         {
@@ -96,56 +29,18 @@ const PlayerChart = ({
 
     const rows = useMemo(() => {
         return playerChart.map((player, index) => {
-            const inningPositionsForPlayer = inningPositions[player.$id] || {};
-            const playerInningPositions = [];
-            for (let i = 1; i <= 7; i++) {
-                const inningKey = `inning${i}`;
-                playerInningPositions.push(inningPositionsForPlayer[inningKey] || player.positions[i - 1] || 'Out');
-            }
 
             const row = {
                 battingOrder: index + 1,
                 playerId: player.$id,
                 player: `${player.firstName} ${player.lastName}`,
             };
-            playerInningPositions.forEach((position, i) => {
-                row[`inning${i + 1}`] = position;
+            player.positions.forEach((position, i) => {
+                row[`inning${i + 1}`] = position || 'Out';
             });
             return row;
         });
-    }, [playerChart, inningPositions]);
-
-    const handlePositionChange = useCallback((position, playerId, inning) => {
-        setInningPositions(prevPositions => {
-            const updatedPositions = { ...prevPositions };
-            if (!updatedPositions[playerId]) {
-                updatedPositions[playerId] = {};
-            }
-            updatedPositions[playerId][inning] = position;
-            return updatedPositions;
-        });
-
-        setPlayerChart(position, playerId, inning);
-
-    }, [setPlayerChart]);
-
-    const getPositionOptions = useCallback((preferredPositions, dislikedPositions) => {
-
-        if (!preferredPositions && !dislikedPositions) {
-            return ['Out', ...Object.keys(fieldingPositions)];
-        }
-
-        const preferred = [...preferredPositions];
-        const disliked = [...dislikedPositions];
-        const nonPreferred = Object.keys(fieldingPositions).filter(position => (!preferred.includes(position) && !disliked.includes(position)));
-
-        return [
-            { group: 'Preferred Positions', items: preferred },
-            { group: 'Other Positions', items: nonPreferred },
-            { group: 'Disliked Positions', items: disliked },
-            'Out',
-        ];
-    }, [fieldingPositions]);
+    }, [playerChart]);
 
     if (!playerChart) {
         return null;
@@ -157,66 +52,37 @@ const PlayerChart = ({
                 mah={450}
                 offsetScrollbars
             >
-                <Table withTableBorder>
+                <Table stickyHeader withTableBorder withColumnBorders striped>
                     <Table.Thead className={styles.header}>
                         <Table.Tr>
-                            {managerView && <Table.Th w={40} />}
                             {columns.map((column) => (
                                 <Table.Th key={column.accessor}>{column.title}</Table.Th>
                             ))}
                         </Table.Tr>
                     </Table.Thead>
-                    <Droppable droppableId="dnd-list" direction="vertical">
-                        {(provided) => (
-                            <Table.Tbody {...provided.droppableProps} ref={provided.innerRef}>
-                                {provided.placeholder}
-                                {rows.map((row, index) => (
-                                    <Draggable key={row.playerId} draggableId={row.playerId} index={index}>
-                                        {(provided) => (
-                                            <Table.Tr ref={provided.innerRef} {...provided.draggableProps}>
-                                                {provided.placeholder}
-                                                {managerView && (
-                                                    <Table.Td>
-                                                        <div {...provided.dragHandleProps}>
-                                                            <IconGripVertical size={18} stroke={1.5} />
-                                                        </div>
-                                                    </Table.Td>
-                                                )}
-                                                {columns.map((column) => {
-                                                    if (column.accessor.startsWith('inning')) {
-                                                        const inning = column.accessor;
-                                                        const player = playerChart.find(p => p.firstName + ' ' + p.lastName === row.player);
+                    <Table.Tbody>
+                        {rows.map((row) => (
+                            <Table.Tr>
+                                {columns.map((column) => {
+                                    if (column.accessor.startsWith('inning')) {
+                                        const position = row[column.accessor];
+                                        let initials = 'Out';
+                                        if (position !== 'Out') initials = fieldingPositions[position].initials;
 
-                                                        const preferredPositions = player?.preferredPositions;
-                                                        const dislikedPositions = player?.dislikedPositions;
-                                                        const positionData = getPositionOptions(preferredPositions, dislikedPositions);
+                                        console.log({ position, initials });
 
-                                                        return (
-                                                            <Table.Td key={column.accessor}>
-                                                                {managerView ? (
-                                                                    <PositionSelect
-                                                                        row={row}
-                                                                        inning={inning}
-                                                                        handlePositionChange={handlePositionChange}
-                                                                        positionData={positionData}
-                                                                        playerChart={playerChart}
-                                                                    />
-                                                                ) : (
-                                                                    <Text>{row[inning]}</Text>
-                                                                )}
-                                                            </Table.Td>
-                                                        );
-                                                    } else {
-                                                        return <Table.Td key={column.accessor}>{row[column.accessor]}</Table.Td>;
-                                                    }
-                                                })}
-                                            </Table.Tr>
-                                        )}
-                                    </Draggable>
-                                ))}
-                            </Table.Tbody>
-                        )}
-                    </Droppable>
+                                        return (
+                                            <Table.Td key={column.accessor}>
+                                                {initials}
+                                            </Table.Td>
+                                        );
+                                    } else {
+                                        return <Table.Td key={column.accessor}>{row[column.accessor]}</Table.Td>;
+                                    }
+                                })}
+                            </Table.Tr>
+                        ))}
+                    </Table.Tbody>
                 </Table>
             </ScrollArea.Autosize>
         </div>
