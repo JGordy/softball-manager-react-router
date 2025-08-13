fe3a8a23884e3f0d02609808e6cafeeb20117776
Joseph Gordy <jgordy2424@gmail.com>
2025-08-13 16:44:08 -0400

    add uvIndex util

---
+++ app/routes/events/utils/getUvIndexColor.js
@@ -0,0 +1,8 @@
+export default function getUvIndexColor(uvi) {
+    const uvIndex = Math.round(uvi);
+    if (uvIndex <= 2) return 'green';
+    if (uvIndex <= 5) return 'yellow';
+    if (uvIndex <= 7) return 'orange';
+    if (uvIndex <= 10) return 'red';
+    return 'purple';
+};
