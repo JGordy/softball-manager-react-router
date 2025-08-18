bcee331d3bd552916e6684af873f4609ab19a72c
Joseph Gordy <jgordy2424@gmail.com>
2025-08-18 16:42:17 -0400

    Convert game time weather to use google data

--- app/routes/events/utils/getGameDateWeather.js
+++ app/routes/events/utils/getGameDateWeather.js
@@ -1,24 +1,16 @@
-import getDailyWeather from './getDailyWeather';
 import getHourlyWeather from './getHourlyWeather';
 
 export default function getGameDateWeather(gameDate, weather) {
-    if (!weather) return {};
+    if (!weather?.hourly) return {};
 
-    if (weather.daily) {
-        return {
-            gameDayWeather: getDailyWeather(weather.daily, gameDate),
-            type: 'daily',
-        };
-    } else if (weather.hourly) {
-        const { hourly, rainout, totalPrecipitation } = getHourlyWeather(weather.hourly, gameDate);
+    const hourlyWeatherResult = getHourlyWeather(weather.hourly, gameDate);
+    if (!hourlyWeatherResult) return {};
 
-        return {
-            gameDayWeather: hourly,
-            rainout,
-            type: 'hourly',
-            totalPrecipitation,
-        };
-    }
+    const { hourly, rainout, totalPrecipitation } = hourlyWeatherResult;
 
-    return {};
+    return {
+        hourly,
+        rainout,
+        totalPrecipitation,
+    };
 }
