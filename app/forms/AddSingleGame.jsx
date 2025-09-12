1a336a2b1f255c94755a94b3c5361d512864145c
Joseph Gordy <jgordy2424@gmail.com>
2025-09-12 09:57:08 -0400

    Update default value for isHomeGame input

--- app/forms/AddSingleGame.jsx
+++ app/forms/AddSingleGame.jsx
@@ -67,7 +67,7 @@ export default function AddSingleGame({
             <Radio.Group
                 mb="md"
                 className={classes.inputs}
-                defaultValue={defaults.isHomegame || "home"}
+                defaultValue={defaults.isHomegame}
                 name="isHomeGame"
                 label="Select the game location"
                 description="Select whether this game is at home or away"
