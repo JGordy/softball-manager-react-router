02e41cf41fe8ee6642c02aab3bc3c841ff92a87c
Joseph Gordy <jgordy2424@gmail.com>
2025-09-12 21:49:50 -0400

    Update jest config and jest packages

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 export default {
     testEnvironment: "jsdom",
     transform: {
+        // Use babel-jest to transpile tests with a Babel preset
         "^.+\\.(t|j)sx?$": "babel-jest",
     },
-    transformIgnorePatterns: ["/node_modules/(?!@mantine|@tabler)"],
+    // Don't transform anything in node_modules except for these packages
+    transformIgnorePatterns: ["/node_modules/(?!(react-router|dnd-core)/)"],
     moduleNameMapper: {
+        // Mock CSS imports
         ".(css|less|scss|sass)": "identity-obj-proxy",
+
+        // Your project path aliases
         "^~/(.*)$": "<rootDir>/app/$1",
         "^@/(.*)$": "<rootDir>/app/$1",
         "^@actions/(.*)$": "<rootDir>/app/actions/$1",
@@ -19,6 +24,18 @@ export default {
         "^@styles/(.*)$": "<rootDir>/app/styles/$1",
         "^@test-utils$": "<rootDir>/app/test-utils/index.js",
         "^@test-utils/(.*)$": "<rootDir>/app/test-utils/$1",
+
+        // --- MANTINE FIX ---
+        // Force Jest to use the CommonJS build for each Mantine package
+        "^@mantine/core$": "<rootDir>/node_modules/@mantine/core/cjs/index.cjs",
+        "^@mantine/dates$":
+            "<rootDir>/node_modules/@mantine/dates/cjs/index.cjs",
+        "^@mantine/hooks$":
+            "<rootDir>/node_modules/@mantine/hooks/cjs/index.cjs",
+        "^@mantine/modals$":
+            "<rootDir>/node_modules/@mantine/modals/cjs/index.cjs",
+        "^@mantine/notifications$":
+            "<rootDir>/node_modules/@mantine/notifications/cjs/index.cjs",
     },
     setupFilesAfterEnv: ["./jest.setup.js"],
 };
