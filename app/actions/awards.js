a74ede7847b1f5aaa9e2a453f6e74c743255a1e5
Joseph Gordy <jgordy2424@gmail.com>
2025-10-03 22:38:26 -0400

    Add try catch to votes request

--- app/actions/awards.js
+++ app/actions/awards.js
@@ -8,23 +8,32 @@ export async function sendAwardVotes({ values, eventId }) {
 
     const categories = votes && Object.keys(votes);
 
-    const promises = categories.map((category) => {
-        const vote = votes[category];
-        if (vote.vote_id) {
-            return updateDocument("votes", vote.vote_id, {
-                nominated_user_id: vote.nominated_user_id,
-            });
-        } else {
-            return createDocument("votes", ID.unique(), {
-                ...rest,
-                game_id: eventId,
-                nominated_user_id: vote.nominated_user_id,
-                reason: category,
-            });
-        }
-    });
+    try {
+        const promises = categories.map((category) => {
+            const vote = votes[category];
+            if (vote.vote_id) {
+                return updateDocument("votes", vote.vote_id, {
+                    nominated_user_id: vote.nominated_user_id,
+                });
+            } else {
+                return createDocument("votes", ID.unique(), {
+                    ...rest,
+                    game_id: eventId,
+                    nominated_user_id: vote.nominated_user_id,
+                    reason: category,
+                });
+            }
+        });
 
-    return await Promise.all(promises);
+        return {
+            success: true,
+            status: 201,
+            response: await Promise.all(promises),
+        };
+    } catch (error) {
+        console.error("Error updating award votes:", error);
+        return { success: false, error: error.message, status: 500 };
+    }
 }
 
 export async function updateAwardVote({ voteId, values }) {
