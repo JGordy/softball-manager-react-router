856a2e8a098ad9e8703ff18f77e6efbdf7dc5fa3
Joseph Gordy <jgordy2424@gmail.com>
2025-10-26 12:34:20 -0400

    Add HomeMenu component for team management interface

---
+++ app/routes/home/components/HomeMenu.jsx
@@ -0,0 +1,34 @@
+import { Text } from "@mantine/core";
+
+import { IconPlus } from "@tabler/icons-react";
+
+import AddTeam from "@/forms/AddTeam";
+
+import useModal from "@/hooks/useModal";
+
+import MenuContainer from "@/components/MenuContainer";
+
+export default function HomeMenu({ userId }) {
+    const { openModal } = useModal();
+
+    const openAddTeamModal = () =>
+        openModal({
+            title: "Add a New Team",
+            children: <AddTeam actionRoute={"/"} userId={userId} />,
+        });
+
+    const sections = [
+        {
+            items: [
+                {
+                    key: "add-team",
+                    onClick: openAddTeamModal,
+                    leftSection: <IconPlus size={14} />,
+                    content: <Text>Add New Team</Text>,
+                },
+            ],
+        },
+    ];
+
+    return <MenuContainer sections={sections} />;
+}
