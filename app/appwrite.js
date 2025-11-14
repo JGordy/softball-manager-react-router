2a069978bb75fbbeec0bee7f85516098f56489c0
Joseph Gordy <jgordy2424@gmail.com>
2025-11-14 13:57:46 -0500

    refactor: remove appwrite.js file as ID and Query utilities are no longer needed

--- app/appwrite.js
+++
@@ -1,3 +0,0 @@
-// Re-export ID and Query utilities from node-appwrite
-// These are pure utility functions that work the same in both client and server contexts
-export { ID, Query } from "node-appwrite";
