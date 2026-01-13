b1ea440bb7dfe11fb1efb2a6e24e81b319d7a7ef
Joseph Gordy <jgordy2424@gmail.com>
2026-01-12 20:57:15 -0500

    feat: add FIELD_CENTROIDS constant for field position coordinates

---
+++ app/routes/events/constants/fieldCentroids.js
@@ -0,0 +1,12 @@
+export const FIELD_CENTROIDS = {
+    Pitcher: { x: 50, y: 62 },
+    Catcher: { x: 50, y: 78 },
+    "First Base": { x: 66, y: 56 },
+    "Second Base": { x: 57, y: 49 },
+    "Third Base": { x: 34, y: 56 },
+    Shortstop: { x: 43, y: 49 },
+    "Left Field": { x: 25, y: 35 },
+    "Left Center Field": { x: 40, y: 25 },
+    "Right Center Field": { x: 60, y: 25 },
+    "Right Field": { x: 75, y: 35 },
+};
