fe8d2340b16c4d90ad55fd91d17042bb590a913f
Joseph Gordy <jgordy2424@gmail.com>
2026-02-19 14:23:00 -0500

    test: add polyfill for crypto.randomUUID in jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -89,6 +89,16 @@ if (typeof globalThis.FormData === "undefined") {
     };
 }
 
+if (typeof window.crypto.randomUUID !== "function") {
+    window.crypto.randomUUID = () => {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+            const r = (Math.random() * 16) | 0;
+            const v = c === "x" ? r : (r & 0x3) | 0x8;
+            return v.toString(16);
+        });
+    };
+}
+
 const { getComputedStyle } = window;
 window.getComputedStyle = (elt) => getComputedStyle(elt);
 window.HTMLElement.prototype.scrollIntoView = () => {};
