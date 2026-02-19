acb12afb60f484820b72735e23c0ac05e058ccdd
Joseph Gordy <jgordy2424@gmail.com>
2026-02-19 11:04:00 -0500

    test: add fileMock for asset file handling in Jest tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -10,6 +10,8 @@ module.exports = {
     // Keep in sync with vite.config.ts and tsconfig.json
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+            "<rootDir>/__mocks__/fileMock.js",
         "^@/(.*)$": "<rootDir>/app/$1",
     },
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
