e89905e39b131a1fcfeff4abf7c68d5de279ed27
Joseph Gordy <jgordy2424@gmail.com>
2026-02-24 10:58:33 -0500

    feat: update theme colors to enhance brand consistency

--- app/theme.js
+++ app/theme.js
@@ -15,9 +15,21 @@ const theme = createTheme({
             "#27a343",
             "#168d36",
         ],
+        lime: [
+            "#f4fce3",
+            "#e9f8c6",
+            "#d4f19b",
+            "#bdea6d",
+            "#a8e446",
+            "#9be02b",
+            "#c1eb33",
+            "#7fbf0a",
+            "#629402",
+            "#416500",
+        ],
     },
-    primaryColor: "green",
-    primaryShade: { light: 8 },
+    primaryColor: "lime",
+    primaryShade: { light: 8, dark: 6 },
     autoContrast: true,
 });
 
