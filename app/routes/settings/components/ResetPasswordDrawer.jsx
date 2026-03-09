621f2a805698006d3e81bec6d0054ddd84c891b8
Joseph Gordy <jgordy2424@gmail.com>
2026-03-09 11:36:40 -0400

    feat: add ResetPasswordDrawer component with tests for rendering and form submission

---
+++ app/routes/settings/components/ResetPasswordDrawer.jsx
@@ -0,0 +1,19 @@
+import DrawerContainer from "@/components/DrawerContainer";
+import UpdatePassword from "@/forms/UpdatePassword";
+
+export default function ResetPasswordDrawer({ opened, onClose, user }) {
+    return (
+        <DrawerContainer
+            opened={opened}
+            onClose={onClose}
+            title="Reset Password"
+        >
+            <UpdatePassword
+                action="password-reset"
+                actionRoute="/settings"
+                confirmText="Yes, Reset my Password"
+                user={user}
+            />
+        </DrawerContainer>
+    );
+}
