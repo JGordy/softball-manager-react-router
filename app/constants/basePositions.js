5397d5ff0596f3d90035c55395bc382768f8f8f6
Joseph Gordy <jgordy2424@gmail.com>
2026-03-26 21:33:20 -0400

    Adjust center base zones

--- app/constants/basePositions.js
+++ app/constants/basePositions.js
@@ -4,9 +4,9 @@
  * relative to a 100x100 container.
  */
 export const BASE_POSITIONS = {
-    "base-home": { x: 50, y: 88 },
-    "base-1": { x: 88, y: 50 },
-    "base-2": { x: 50, y: 12 },
-    "base-3": { x: 12, y: 50 },
+    "base-home": { x: 50, y: 91 },
+    "base-1": { x: 91, y: 50 },
+    "base-2": { x: 50, y: 9 },
+    "base-3": { x: 9, y: 50 },
     "out-zone": { x: 85, y: 85 },
 };
